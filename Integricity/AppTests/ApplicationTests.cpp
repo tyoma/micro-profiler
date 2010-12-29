37f4f4b5d85b31eb92b1f80d932179627590b7f1
Artem Gevorkyan <artem.gevorkyan@gmail.com>
2010-12-29 09:02:36 +0000

    git-svn-id: http://micro-profiler.googlecode.com/svn/trunk@151 1676f2fc-95af-a39c-d699-6f41337c5309

---
+++ Integricity/AppTests/ApplicationTests.cpp
@@ -0,0 +1,20 @@
+using namespace System;
+using namespace System::Text;
+using namespace System::Collections::Generic;
+using namespace Microsoft::VisualStudio::TestTools::UnitTesting;
+
+namespace AppTests
+{
+	[TestClass]
+	public ref class ApplicationTests
+	{
+	public: 
+		[TestMethod]
+		void Smoke()
+		{
+			//
+			// TODO: Add test logic here
+			//
+		};
+	};
+}

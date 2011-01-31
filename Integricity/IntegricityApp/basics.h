b184549a05dafcaedc59b5e7fe81cd4304bd23bd
Artem Gevorkyan <artem.gevorkyan@gmail.com>
2011-01-31 13:58:11 +0000

    - utility abstraction of entries file added to tests.
    git-svn-id: http://micro-profiler.googlecode.com/svn/trunk@165 1676f2fc-95af-a39c-d699-6f41337c5309

--- Integricity/IntegricityApp/basics.h
+++ Integricity/IntegricityApp/basics.h
@@ -1,5 +1,11 @@
 #pragma once
 
+class noncopyable
+{
+	noncopyable(const noncopyable &other);
+	const noncopyable &operator =(const noncopyable &rhs);
+};
+
 struct destructible
 {
 	virtual ~destructible() throw()	{	}

4fdd6ac40af00b000785fc99a829fea77640693b
artemg <artemg@1676f2fc-95af-a39c-d699-6f41337c5309>
2011-03-12 19:22:26 +0000

    - some basic notifications are fired when cvs directory changes.

--- trunk/Integricity/AppTests/CVSSCTests.cpp
+++ trunk/Integricity/AppTests/CVSSCTests.cpp
@@ -18,17 +18,19 @@ namespace AppTests
 	struct stub_listener : public repository::listener
 	{
 		stub_listener()
-			: notification_arrived(false)
+			: notification_arrived(false), calls(0)
 		{	}
 
 		virtual void modified(const vector< pair<wstring, repository::state> > &modifications)
 		{
-			notification_arrived.set();
+			++calls;
 			this->modifications.insert(this->modifications.end(), modifications.begin(), modifications.end());
+			notification_arrived.set();
 		}
 
 		event_monitor notification_arrived;
 		vector< pair<wstring, repository::state> > modifications;
+		int calls;
 	};
 
 	[TestClass]
@@ -250,7 +252,7 @@ namespace AppTests
 		}
 
 
-//		[TestMethod]
+		[TestMethod]
 		void NotifyOnFileUpdate()
 		{
 			// INIT
@@ -296,6 +298,107 @@ namespace AppTests
 			// ASSERT
 			Assert::IsTrue(1 == l.modifications.size());
 			Assert::IsTrue(make_pair(wstring(L"thread.h"), repository::state_modified) == l.modifications[0]);
+		}
+
+
+		[TestMethod]
+		void NotifyOnSequentialFileUpdates()
+		{
+			// INIT
+			stub_listener l;
+			temp_directory d(L"sample");
+			DateTime dt1(2010, 7, 7, 15, 50, 26, DateTimeKind::Utc);
+			DateTime dt2(2008, 3, 12, 12, 51, 13, DateTimeKind::Utc);
+			DateTime dt3(2010, 11, 29, 17, 18, 4, DateTimeKind::Utc);
+			DateTime dt4(2011, 1, 29, 17, 18, 4, DateTimeKind::Utc);
+
+			{
+				entries_file e(d.path());
+
+				e.append(L"thread.cpp", L"1.4", dt1);
+				e.append(L"thread.h", L"1.5", dt2);
+				e.append(L"mutex.cpp", L"1.6", dt3);
+				e.append(L"mutex.h", L"1.7", dt4);
+			}
+
+			File::Create(make_managed(d.path() / L"thread.cpp"))->Close();
+			File::Create(make_managed(d.path() / L"thread.h"))->Close();
+			File::Create(make_managed(d.path() / L"mutex.cpp"))->Close();
+			File::Create(make_managed(d.path() / L"mutex.h"))->Close();
+			File::SetLastWriteTimeUtc(make_managed(d.path() / L"thread.cpp"), dt1);
+			File::SetLastWriteTimeUtc(make_managed(d.path() / L"thread.h"), dt2);
+			File::SetLastWriteTimeUtc(make_managed(d.path() / L"mutex.cpp"), dt3);
+			File::SetLastWriteTimeUtc(make_managed(d.path() / L"mutex.h"), dt4);
+
+			shared_ptr<repository> r = repository::create_cvs_sc(d.path(), l);
+
+			// ACT / ASSERT
+			{
+				StreamWriter ^s = File::AppendText(make_managed(d.path() / L"thread.cpp"));
+
+				s->WriteLine("some new line");
+				delete s;
+			}
+			Assert::IsTrue(waitable::satisfied == l.notification_arrived.wait(waitable::infinite));
+
+			// ASSERT
+			Assert::IsTrue(1 == l.calls);
+			Assert::IsTrue(1 == l.modifications.size());
+			Assert::IsTrue(make_pair(wstring(L"thread.cpp"), repository::state_modified) == l.modifications[0]);
+
+			// INIT
+			l.modifications.clear();
+			l.calls = 0;
+			l.notification_arrived.reset();
+
+			// ACT / ASSERT
+			{
+				StreamWriter ^s = File::AppendText(make_managed(d.path() / L"mutex.cpp"));
+
+				s->WriteLine("some new line");
+				delete s;
+			}
+			Assert::IsTrue(waitable::satisfied == l.notification_arrived.wait(waitable::infinite));
+
+			// ASSERT
+			Assert::IsTrue(1 == l.calls);
+			Assert::IsTrue(2 == l.modifications.size());
+			Assert::IsTrue(make_pair(wstring(L"mutex.cpp"), repository::state_modified) == l.modifications[0]);
+			Assert::IsTrue(make_pair(wstring(L"thread.cpp"), repository::state_modified) == l.modifications[1]);
+		}
+
+
+		[TestMethod]
+		void NotifyOnNewUnversionedFile()
+		{
+			// INIT
+			stub_listener l;
+			temp_directory d(L"sample");
+			DateTime dt1(2010, 7, 7, 15, 50, 26, DateTimeKind::Utc);
+			DateTime dt2(2008, 3, 12, 12, 51, 13, DateTimeKind::Utc);
+
+			{
+				entries_file e(d.path());
+
+				e.append(L"thread.cpp", L"1.4", dt1);
+				e.append(L"thread.h", L"1.5", dt2);
+			}
+
+			File::Create(make_managed(d.path() / L"thread.cpp"))->Close();
+			File::Create(make_managed(d.path() / L"thread.h"))->Close();
+			File::SetLastWriteTimeUtc(make_managed(d.path() / L"thread.cpp"), dt1);
+			File::SetLastWriteTimeUtc(make_managed(d.path() / L"thread.h"), dt2);
+
+			shared_ptr<repository> r = repository::create_cvs_sc(d.path(), l);
+
+			// ACT
+			File::Create(make_managed(d.path() / L"process.h"))->Close();
+
+			// ASSERT
+			Assert::IsTrue(waitable::satisfied == l.notification_arrived.wait(waitable::infinite));
+
+			Assert::IsTrue(1 == l.modifications.size());
+			Assert::IsTrue(make_pair(wstring(L"process.h"), repository::state_unversioned) == l.modifications[0]);
 		}
 	};
 }

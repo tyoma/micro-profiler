abd31fe6ee771bd86b1fe55b2d1f2d245d3ed141
Artem Gevorkyan <artem.gevorkyan@gmail.com>
2011-03-18 16:41:31 +0000

    - message interception implemented.
    git-svn-id: http://micro-profiler.googlecode.com/svn/trunk@183 1676f2fc-95af-a39c-d699-6f41337c5309

--- Integricity/IntegricityApp/windowing.cpp
+++ Integricity/IntegricityApp/windowing.cpp
@@ -4,16 +4,30 @@
 #include <stdexcept>
 
 using namespace std;
+using namespace placeholders;
 
 namespace
 {
-	const TCHAR c_wrapper_ptr_name[] = _T("IntegricityWrapper");
+	const TCHAR c_wrapper_ptr_name[] = _T("IntegricityWrapperPtr");
+
+	class disconnector : noncopyable, public destructible
+	{
+		shared_ptr<window_wrapper> _target;
+
+	public:
+		disconnector(shared_ptr<window_wrapper> target)
+			: _target(target)
+		{	}
+
+		~disconnector() throw()
+		{	_target->unadvise();	}
+	};
 }
 
 window_wrapper::window_wrapper(HWND hwnd)
 	: _window(hwnd)
 {
-	_previous = reinterpret_cast<WNDPROC>(::SetWindowLongPtr(hwnd, GWL_WNDPROC, reinterpret_cast<LONG>(&window_wrapper::windowproc_proxy)));
+	_previous_handler = reinterpret_cast<WNDPROC>(::SetWindowLongPtr(hwnd, GWL_WNDPROC, reinterpret_cast<LONG>(&window_wrapper::windowproc_proxy)));
 	::SetProp(hwnd, c_wrapper_ptr_name, reinterpret_cast<HANDLE>(this));
 }
 
@@ -21,7 +35,12 @@ LRESULT CALLBACK window_wrapper::windowproc_proxy(HWND hwnd, UINT message, WPARA
 {
 	shared_ptr<window_wrapper> w(window_wrapper::extract(hwnd));
 
-	LRESULT result = w->_previous(hwnd, message, wparam, lparam);
+	if (message == WM_NCDESTROY)
+		::RemoveProp(hwnd, c_wrapper_ptr_name);
+
+	LRESULT result = w->_user_handler != 0 ? (*w->_user_handler)(message, wparam, lparam, bind(w->_previous_handler, w->hwnd(), _1, _2, _3))
+			: w->_previous_handler(hwnd, message, wparam, lparam);
+
 	if (message == WM_NCDESTROY)
 		w->_this.reset();
 	return result;
@@ -50,6 +69,15 @@ shared_ptr<window_wrapper> window_wrapper::attach(HWND hwnd)
 	else
 		throw invalid_argument("");
 }
+
+shared_ptr<destructible> window_wrapper::advise(const user_handler_t &user_handler)
+{
+	_user_handler.reset(new user_handler_t(user_handler));
+	return shared_ptr<destructible>(new disconnector(shared_from_this()));
+}
+
+void window_wrapper::unadvise() throw()
+{	_user_handler.reset();	}
 
 HWND window_wrapper::hwnd() const
 {	return _window;	}

058c0c0612b4bab7c2b528aee51c92a83448b595
Artem Gevorkyan <artem.gevorkyan@gmail.com>
2011-03-21 22:28:49 +0000

    - treeview_handler refactored to a function; - prepaint message handling implementation began.
    git-svn-id: http://micro-profiler.googlecode.com/svn/trunk@187 1676f2fc-95af-a39c-d699-6f41337c5309

--- Integricity/IntegricityApp/treeview_handler.h
+++ Integricity/IntegricityApp/treeview_handler.h
@@ -2,12 +2,15 @@
 
 #include "basics.h"
 
+#include <functional>
 #include <memory>
+#include <vector>
 
-class treeview_handler : noncopyable
-{
-	std::shared_ptr<destructible> _interception;
+typedef std::function<void (
+	const std::vector<std::wstring> &/*item_path*/,
+	unsigned int &/*foreground_color*/,
+	unsigned int &/*background_color*/,
+	bool &/*emphasized*/
+)> prepaint_handler_t;
 
-public:
-	treeview_handler(void *htree);
-};
+std::shared_ptr<destructible> handle_tv_notifications(void *htree, const prepaint_handler_t &prepaint_handler);

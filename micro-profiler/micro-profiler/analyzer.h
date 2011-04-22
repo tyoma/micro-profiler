6d1e60278a6bfea32857490fe323c22564a5a55f
artemg <artemg@1676f2fc-95af-a39c-d699-6f41337c5309>
2011-04-22 18:50:08 +0000

    - broken commit: to continue from home.

--- micro-profiler/micro-profiler/analyzer.h
+++ micro-profiler/micro-profiler/analyzer.h
@@ -2,21 +2,32 @@
 
 #include "calls_collector.h"
 
+#include <hash_map>
+
 namespace micro_profiler
 {
+	struct function_statistics
+	{
+		unsigned __int64 times_called, inclusive_time, exclusive_time;
+	};
+
 	class analyzer : public calls_collector::acceptor
 	{
+		typedef stdext::hash_map<void * /*function_ptr*/, function_statistics> statistics_container;
+
+		statistics_container _statistics;
+
 	public:
-		bool begin() const;
-		bool end() const;
+		statistics_container::const_iterator begin() const;
+		statistics_container::const_iterator end() const;
 
 		__declspec(dllexport) virtual void accept_calls(unsigned int threadid, const call_record *calls, unsigned int count);
 	};
 
 
-	inline bool analyzer::begin() const
-	{	return true;	}
+	inline analyzer::statistics_container::const_iterator analyzer::begin() const
+	{	return _statistics.begin();	}
 
-	inline bool analyzer::end() const
-	{	return true;	}
+	inline analyzer::statistics_container::const_iterator analyzer::end() const
+	{	return _statistics.begin();	}
 }
--- micro-profiler/tests/AnalyzerTests.cpp
+++ micro-profiler/tests/AnalyzerTests.cpp
@@ -2,6 +2,9 @@
 
 #include <pod_vector.h>
 
+#include <map>
+
+using namespace std;
 using namespace System;
 using namespace Microsoft::VisualStudio::TestTools::UnitTesting;
 
@@ -41,7 +44,7 @@ namespace tests
 			micro_profiler::pod_vector<micro_profiler::call_record> trace;
 
 			trace.append(_call_record((void *)1234, 12300));
-			trace.append(_call_record((void *)2234, 12300));
+			trace.append(_call_record((void *)2234, 12305));
 
 			// ACT
 			as_acceptor.accept_calls(1, trace.data(), trace.size());
@@ -49,6 +52,39 @@ namespace tests
 
 			// ASSERT
 			Assert::IsTrue(a.begin() == a.end());
+		}
+
+
+		[TestMethod]
+		void EvaluateSeparateNonNestingFunctionDurations()
+		{
+			// INIT
+			micro_profiler::analyzer a;
+			micro_profiler::pod_vector<micro_profiler::call_record> trace;
+
+			trace.append(_call_record((void *)1234, 12300));
+			trace.append(_call_record(0, 12305));
+			trace.append(_call_record((void *)2234, 12310));
+			trace.append(_call_record(0, 12317));
+
+			// ACT
+			a.accept_calls(1, trace.data(), trace.size());
+
+			// ASSERT
+			map<void *, micro_profiler::function_statistics> m(a.begin(), a.end());	// use map to ensure proper sorting
+			map<void *, micro_profiler::function_statistics>::const_iterator f1(m.begin()), f2(m.begin());
+
+			++f2;
+
+			Assert::IsTrue(2 == m.size());
+
+			Assert::IsTrue(1 == f1->second.times_called);
+			Assert::IsTrue(5 == f1->second.inclusive_time);
+			Assert::IsTrue(5 == f1->second.exclusive_time);
+
+			Assert::IsTrue(1 == f2->second.times_called);
+			Assert::IsTrue(7 == f2->second.inclusive_time);
+			Assert::IsTrue(7 == f2->second.exclusive_time);
 		}
 	};
 }

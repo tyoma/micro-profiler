4dddddd4a53adc9d41b82a0da7a498427cbd1228
artemg <artemg@1676f2fc-95af-a39c-d699-6f41337c5309>
2011-04-23 20:30:03 +0000

    - less spaces, more tabs

--- micro-profiler/micro-profiler/shadow_stack.h
+++ micro-profiler/micro-profiler/shadow_stack.h
@@ -8,13 +8,13 @@ namespace micro_profiler
 {
 	class shadow_stack
 	{
-      struct call_record_ex : call_record
-      {
-         call_record_ex(const call_record &from);
-         call_record_ex(const call_record_ex &other);
+		struct call_record_ex : call_record
+		{
+			call_record_ex(const call_record &from);
+			call_record_ex(const call_record_ex &other);
 
-         unsigned __int64 child_time;
-      };
+			unsigned __int64 child_time;
+		};
 
 		std::vector<call_record_ex> _stack;
 
@@ -46,11 +46,11 @@ namespace micro_profiler
 	}
 
 
-   inline shadow_stack::call_record_ex::call_record_ex(const call_record &from)
-      : call_record(from), child_time(0)
-   {  }
+	inline shadow_stack::call_record_ex::call_record_ex(const call_record &from)
+		: call_record(from), child_time(0)
+	{	}
 
-   inline shadow_stack::call_record_ex::call_record_ex(const call_record_ex &other)
-      : call_record(other), child_time(other.child_time)
-   {  }
+	inline shadow_stack::call_record_ex::call_record_ex(const call_record_ex &other)
+		: call_record(other), child_time(other.child_time)
+	{	}
 }

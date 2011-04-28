f5f44f86aa29d75b5782215f08e83a33b563f7e8
artemg <artemg@1676f2fc-95af-a39c-d699-6f41337c5309>
2011-04-28 15:53:15 +0000

    - some drafts on function stat display.

--- micro-profiler/micro-profiler-frontend/ProfilerMainDialog.cpp
+++ micro-profiler/micro-profiler-frontend/ProfilerMainDialog.cpp
@@ -1,7 +1,24 @@
 #include "ProfilerMainDialog.h"
 
 ProfilerMainDialog::ProfilerMainDialog()
-{	Create(NULL, 0);	}
+{
+	Create(NULL, 0);
+	_statistics_view = GetDlgItem(IDC_FUNCTIONS_STATISTICS);
+
+	LVCOLUMN columns[] = {
+		{ LVCF_TEXT | LVCF_SUBITEM | LVCF_ORDER | LVCF_WIDTH, 0, 60, _T("Function"), 0, 0, 0, 0,	},
+		{ LVCF_TEXT | LVCF_SUBITEM | LVCF_ORDER | LVCF_WIDTH, 0, 70, _T("Times Called"), 0, 1, 0, 1,	},
+		{ LVCF_TEXT | LVCF_SUBITEM | LVCF_ORDER | LVCF_WIDTH, 0, 70, _T("Exclusive Time"), 0, 2, 0, 2,	},
+		{ LVCF_TEXT | LVCF_SUBITEM | LVCF_ORDER | LVCF_WIDTH, 0, 70, _T("Inclusive Time"), 0, 3, 0, 3,	},
+	};
+
+	ListView_InsertColumn(_statistics_view, 0, &columns[0]);
+	ListView_InsertColumn(_statistics_view, 1, &columns[1]);
+	ListView_InsertColumn(_statistics_view, 2, &columns[2]);
+	ListView_InsertColumn(_statistics_view, 3, &columns[3]);
+	ListView_SetExtendedListViewStyle(_statistics_view, LVS_EX_FULLROWSELECT | ListView_GetExtendedListViewStyle(_statistics_view));
+	ListView_SetItemCountEx(_statistics_view, 10000, LVSICF_NOSCROLL);
+}
 
 ProfilerMainDialog::~ProfilerMainDialog()
 {
@@ -9,9 +26,22 @@ ProfilerMainDialog::~ProfilerMainDialog()
 		DestroyWindow();
 }
 
-	LRESULT ProfilerMainDialog::OnInitDialog(UINT /*message*/, WPARAM /*wparam*/, LPARAM /*lparam*/, BOOL& handled)
+LRESULT ProfilerMainDialog::OnInitDialog(UINT /*message*/, WPARAM /*wparam*/, LPARAM /*lparam*/, BOOL& handled)
+{
+	//		CDialogImpl<ProfilerMainDialog>::OnInitDialog(uMsg, wParam, lParam, bHandled);
+	handled = TRUE;
+	return 1;  // Let the system set the focus
+}
+
+LRESULT ProfilerMainDialog::OnGetDispInfo(int control_id, LPNMHDR pnmh, BOOL &handled)
+{
+	NMLVDISPINFO *pdi = (NMLVDISPINFO*)pnmh;
+
+	if (LVIF_TEXT & pdi->item.mask)
 	{
-//		CDialogImpl<ProfilerMainDialog>::OnInitDialog(uMsg, wParam, lParam, bHandled);
+		_tcsncpy(pdi->item.pszText, _T("abc"), pdi->item.cchTextMax - 1);
+		pdi->item.pszText[pdi->item.cchTextMax - 1] = _T('\0');
 		handled = TRUE;
-		return 1;  // Let the system set the focus
 	}
+	return 0;
+}
--- micro-profiler/micro-profiler-frontend/ProfilerMainDialog.h
+++ micro-profiler/micro-profiler-frontend/ProfilerMainDialog.h
@@ -7,6 +7,8 @@
 
 class ProfilerMainDialog : public ATL::CDialogImpl<ProfilerMainDialog>
 {
+   CWindow _statistics_view;
+
 public:
 	ProfilerMainDialog();
 	~ProfilerMainDialog();
@@ -15,6 +17,7 @@ public:
 
 BEGIN_MSG_MAP(ProfilerMainDialog)
 	MESSAGE_HANDLER(WM_INITDIALOG, OnInitDialog)
+	NOTIFY_RANGE_CODE_HANDLER(IDC_FUNCTIONS_STATISTICS, IDC_FUNCTIONS_STATISTICS, LVN_GETDISPINFO, OnGetDispInfo)
 //	COMMAND_HANDLER(IDOK, BN_CLICKED, OnClickedOK)
 //	CHAIN_MSG_MAP(CAxDialogImpl<ProfilerMainDialog>)
 END_MSG_MAP()
@@ -24,5 +27,7 @@ END_MSG_MAP()
 //  LRESULT CommandHandler(WORD wNotifyCode, WORD wID, HWND hWndCtl, BOOL& bHandled);
 //  LRESULT NotifyHandler(int idCtrl, LPNMHDR pnmh, BOOL& bHandled);
 
-	LRESULT OnInitDialog(UINT message, WPARAM wparam, LPARAM lparam, BOOL& handled);
+	LRESULT OnInitDialog(UINT message, WPARAM wparam, LPARAM lparam, BOOL &handled);
+
+   LRESULT OnGetDispInfo(int control_id, LPNMHDR pnmh, BOOL &handled);
 };

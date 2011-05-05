3903618a72c72d8012bd054cefd3c77a49d49dcd
artemg <artemg@1676f2fc-95af-a39c-d699-6f41337c5309>
2011-05-05 09:27:20 +0000

    - profiler_frontend class slightly refactored; - psapi.lib reference removed.

--- micro-profiler/micro-profiler/entry.cpp
+++ micro-profiler/micro-profiler/entry.cpp
@@ -52,8 +52,9 @@ namespace micro_profiler
 	}
 
 	profiler_frontend::profiler_frontend(frontend_factory factory)
-		: _factory(factory ? factory : &create_standard_frontend), _stop_event(::CreateEvent(NULL, TRUE, FALSE, NULL)),
-		_frontend_thread(reinterpret_cast<void *>(_beginthreadex(0, 0, &profiler_frontend::frontend_proc, this, 0, 0)))
+		: _collector(*calls_collector::instance()), _factory(factory ? factory : &create_standard_frontend),
+			_stop_event(::CreateEvent(NULL, TRUE, FALSE, NULL)),
+			_frontend_thread(reinterpret_cast<void *>(_beginthreadex(0, 0, &frontend_worker_proxy, this, 0, 0)))
 	{	}
 
 	profiler_frontend::~profiler_frontend()
@@ -64,42 +65,41 @@ namespace micro_profiler
 		::CloseHandle(reinterpret_cast<HANDLE>(_stop_event));
 	}
 
-	unsigned int __stdcall profiler_frontend::frontend_proc(void *param)
+	unsigned int __stdcall profiler_frontend::frontend_worker_proxy(void *param)
 	{
-		profiler_frontend *_this = reinterpret_cast<profiler_frontend *>(param);
-
 		::SetThreadPriority(::GetCurrentThread(), THREAD_PRIORITY_TIME_CRITICAL);
-		CoInitialize(NULL);
+		::CoInitialize(NULL);
+		reinterpret_cast<profiler_frontend *>(param)->frontend_worker();
+		::CoUninitialize();
+		return 0;
+	}
+
+	void profiler_frontend::frontend_worker()
+	{
+		analyzer a(_collector.profiler_latency());
+		vector<FunctionStatistics> buffer;
+		CComPtr<IProfilerFrontend> fe;
+		TCHAR image_path[MAX_PATH + 1] = { 0 };
+
+		_factory(&fe);
+		if (fe)
 		{
-			calls_collector *collector = calls_collector::instance();
-			analyzer a(collector->profiler_latency());
-			vector<FunctionStatistics> buffer;
-			CComPtr<IProfilerFrontend> fe;
-			TCHAR image_path[MAX_PATH + 1] = { 0 };
-
-			_this->_factory(&fe);
-			if (fe)
+			::GetModuleFileName(NULL, image_path, MAX_PATH);
+			fe->Initialize(CComBSTR(image_path), reinterpret_cast<__int64>(::GetModuleHandle(NULL)), c_ticks_resolution);
+			while (WAIT_TIMEOUT == ::WaitForSingleObject(reinterpret_cast<HANDLE>(_stop_event), 10))
 			{
-				::GetModuleFileName(NULL, image_path, MAX_PATH);
-				fe->Initialize(CComBSTR(image_path), reinterpret_cast<__int64>(::GetModuleHandle(NULL)), c_ticks_resolution);
-				while (WAIT_TIMEOUT == ::WaitForSingleObject(reinterpret_cast<HANDLE>(_this->_stop_event), 10))
+				a.clear();
+				buffer.clear();
+				_collector.read_collected(a);
+				for (analyzer::const_iterator i = a.begin(); i != a.end(); ++i)
 				{
-					a.clear();
-					buffer.clear();
-					collector->read_collected(a);
-					for (analyzer::const_iterator i = a.begin(); i != a.end(); ++i)
-					{
-						FunctionStatistics s = { reinterpret_cast<hyper>(i->first) - 5, i->second.times_called, i->second.exclusive_time, i->second.inclusive_time };
+					FunctionStatistics s = { reinterpret_cast<hyper>(i->first) - 5, i->second.times_called, i->second.exclusive_time, i->second.inclusive_time };
 
-						buffer.push_back(s);
-					}
-					if (!buffer.empty())
-						fe->UpdateStatistics(static_cast<long>(buffer.size()), &buffer[0]);
+					buffer.push_back(s);
 				}
+				if (!buffer.empty())
+					fe->UpdateStatistics(static_cast<long>(buffer.size()), &buffer[0]);
 			}
 		}
-		CoUninitialize();
-
-		return 0;
 	}
 }
--- micro-profiler/micro-profiler/entry.h
+++ micro-profiler/micro-profiler/entry.h
@@ -5,13 +5,16 @@ struct IProfilerFrontend;
 namespace micro_profiler
 {
 	typedef void (*frontend_factory)(IProfilerFrontend **frontend);
+	class calls_collector;
 
 	class profiler_frontend
 	{
+		calls_collector &_collector;
 		frontend_factory _factory;
 		void *_stop_event, *_frontend_thread;
 
-		static unsigned int __stdcall profiler_frontend::frontend_proc(void *param);
+		static unsigned int __stdcall frontend_worker_proxy(void *param);
+		void frontend_worker();
 
 		profiler_frontend(const profiler_frontend &);
 		void operator =(const profiler_frontend &);

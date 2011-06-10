357bfa9380fe742c933c2e2d0a568606d9b2e2bb
artemg <artemg@1676f2fc-95af-a39c-d699-6f41337c5309>
2011-06-10 10:45:46 +0000

    - initial version.

---
+++ trunk/libraries/os/mt.h
@@ -0,0 +1,42 @@
+#pragma once
+
+#include <functional>
+
+namespace std
+{
+   using tr1::function;
+}
+
+namespace mt
+{
+	struct waitable
+	{
+		enum wait_status { satisfied, timeout, abandoned };
+
+		static const unsigned int infinite = -1;
+
+		virtual wait_status wait(unsigned int timeout) volatile = 0;
+	};
+
+	class event_monitor : public waitable
+	{
+		void *_handle;
+
+	public:
+		explicit event_monitor(bool auto_reset);
+		~event_monitor();
+
+		void set();
+		void reset();
+		virtual wait_status wait(unsigned int timeout) volatile;
+	};
+
+	class thread
+	{
+		void *_thread;
+
+	public:
+		thread(const std::function<void()> &job);
+		virtual ~thread() throw();
+	};
+}

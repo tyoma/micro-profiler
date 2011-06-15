f0531c0d88758eaa40227b52181e6dbc1f229149
artemg <artemg@1676f2fc-95af-a39c-d699-6f41337c5309>
2011-06-15 11:14:52 +0000

    - reintegrated changes from trunk.

---
+++ branch/micro-profiler.experimental/micro-profiler.tests/run-tests.vs10.cmd
@@ -0,0 +1,1 @@
+"%VS100COMNTOOLS%\..\ide\mstest.exe" /testcontainer:micro-profiler.tests.dll /testcontainer:oslib.tests.dll
---
+++ branch/micro-profiler.experimental/micro-profiler.tests/run-tests.vs9.cmd
@@ -0,0 +1,1 @@
+"%VS90COMNTOOLS%\..\ide\mstest.exe" /testcontainer:micro-profiler.tests.dll /testcontainer:oslib.tests.dll

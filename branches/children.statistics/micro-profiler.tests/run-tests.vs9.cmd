b3be57786703b21ed3a9d9d97a88833933593f7e
artem.gevorkyan@gmail.com <artem.gevorkyan@gmail.com@1676f2fc-95af-a39c-d699-6f41337c5309>
2011-07-15 06:36:35 +0000

    - changes from trunk reintegrated.

--- branches/children.statistics/micro-profiler.tests/run-tests.vs9.cmd
+++ branches/children.statistics/micro-profiler.tests/run-tests.vs9.cmd
@@ -1,1 +1,1 @@
-"%VS90COMNTOOLS%\..\ide\mstest.exe" /testcontainer:micro-profiler.tests.dll /testcontainer:oslib.tests.dll
+"%VS90COMNTOOLS%\..\ide\mstest.exe" /testcontainer:micro-profiler.tests.dll /testcontainer:mt.tests.dll

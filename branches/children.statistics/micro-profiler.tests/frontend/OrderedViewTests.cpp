60ecf09ffd61516c12e686ac0e0d39e01d7fd1fa
artem.gevorkyan@gmail.com <artem.gevorkyan@gmail.com@1676f2fc-95af-a39c-d699-6f41337c5309>
2011-07-21 12:18:16 +0000

    - changes from trunk reintegrated.

---
+++ branches/children.statistics/micro-profiler.tests/frontend/OrderedViewTests.cpp
@@ -0,0 +1,515 @@
+
+#include <frontend/ordered_view.h>
+
+#include <hash_map>
+#include <utility>
+
+namespace
+{
+	struct POD
+	{
+		int a;
+		int b;
+		double c;
+	};
+
+	bool operator== (const POD &left, const POD &right)
+	{
+		return left.a == right.a && left.b == right.b && left.c == right.c;
+	}
+
+
+	typedef stdext::hash_map<void *, POD> pod_map;
+	typedef ordered_view<pod_map> sorted_pods;
+
+	std::pair<void *const, POD> make_pod(const POD &pod)
+	{
+		return std::make_pair((void *)&pod, pod);
+	}
+
+	bool sort_by_a(const void *const, const POD &left, const void *const, const POD &right)
+	{
+		return left.a > right.a;
+	}
+
+	struct sort_by_b
+	{
+		bool operator()(const void *const, const POD &left, const void *const, const POD &right) const
+		{
+			return left.b > right.b;
+		}
+	};
+
+	bool sort_by_c(const void *const, const POD &left, const void *const, const POD &right)
+	{
+		return left.c > right.c;
+	}
+}
+
+//using namespace std;
+using namespace Microsoft::VisualStudio::TestTools::UnitTesting;
+
+namespace micro_profiler
+{
+	namespace tests
+	{
+		[TestClass]
+		public ref class OrderedViewTests
+		{
+		public: 
+
+			[TestMethod]
+			void CanCreateEmptyOrderedView()
+			{
+				pod_map source;
+
+				sorted_pods s(source);
+				Assert::IsTrue(source.size() == 0);
+				Assert::IsTrue(s.size() == 0);
+			}
+
+			[TestMethod]
+			void CanUseEmptyOrderedView()
+			{
+				pod_map source;
+				
+				POD dummy = {1, 1, 1.0}; // just for find_by_key
+
+				sorted_pods s(source);
+
+				s.set_order(&sort_by_a, true);
+				Assert::IsTrue(s.size() == 0);
+				Assert::IsTrue(s.find_by_key(&dummy) == sorted_pods::npos);
+				
+				s.set_order(&sort_by_a, false);
+				Assert::IsTrue(s.size() == 0);
+				Assert::IsTrue(s.find_by_key(&dummy) == sorted_pods::npos);
+
+				s.set_order(sort_by_b(), false);
+				Assert::IsTrue(s.size() == 0);
+				Assert::IsTrue(s.find_by_key(&dummy) == sorted_pods::npos);
+
+				Assert::IsTrue(source.size() == 0);
+			}
+
+			[TestMethod]
+			void OrderedViewPreserveSize()
+			{
+				pod_map source;
+
+				POD pod1 = {1, 10, 0.5};
+				POD pod2 = {15, 1, 0.6};
+
+				source[&pod1] = pod1;
+				source[&pod2] = pod2;
+
+				sorted_pods s(source);
+				Assert::IsTrue(s.size() == source.size());
+
+				s.set_order(&sort_by_a, true);
+				Assert::IsTrue(s.size() == source.size());
+
+			}
+
+			[TestMethod]
+			void OrderedViewPreserveMapOrderWithoutPredicate()
+			{
+				pod_map source;
+
+				POD pod1 = {1, 10, 0.5};
+				POD pod2 = {15, 3, 1.6};
+				POD pod3 = {5, 5, 5};
+				POD pod4 = {25, 1, 2.3};
+
+				source[&pod1] = pod1;
+				source[&pod2] = pod2;
+				source[&pod3] = pod3;
+				source[&pod4] = pod4;
+
+
+				sorted_pods s(source);
+				Assert::IsTrue(s.size() == source.size());
+				int i = 0;
+				pod_map::const_iterator it = source.begin();
+				for (; it != source.end(); ++it, ++i)
+				{
+					Assert::IsTrue((*it) == s.at(i));
+				}
+
+			}
+
+			[TestMethod]
+			void OrderedViewSortsCorrectlyAsc()
+			{
+				pod_map source;
+
+				POD biggestA = {15, 1, 0.6};
+				POD biggestB = {1, 10, 0.5};
+				POD biggestC = {14, 1, 1.6};
+
+				source[&biggestA] = biggestA;
+				source[&biggestB] = biggestB;
+				source[&biggestC] = biggestC;
+
+				sorted_pods s(source);
+				s.set_order(&sort_by_a, true);
+				
+				Assert::IsTrue(s.at(0) == make_pod(biggestA));
+				Assert::IsTrue(s.at(1) == make_pod(biggestC));
+				Assert::IsTrue(s.at(2) == make_pod(biggestB));
+			}
+
+			[TestMethod]
+			void OrderedViewSortsCorrectlyDesc()
+			{
+				pod_map source;
+
+				POD biggestA = {15, 1, 0.6};
+				POD biggestB = {1, 10, 0.5};
+				POD biggestC = {14, 1, 1.6};
+
+				source[&biggestA] = biggestA;
+				source[&biggestB] = biggestB;
+				source[&biggestC] = biggestC;
+
+				sorted_pods s(source);
+				s.set_order(&sort_by_a, false);
+
+				Assert::IsTrue(s.at(0) == make_pod(biggestB));
+				Assert::IsTrue(s.at(1) == make_pod(biggestC));
+				Assert::IsTrue(s.at(2) == make_pod(biggestA));
+			}
+
+			[TestMethod]
+			void OrderedViewCanSwitchPredicate()
+			{
+				pod_map source;
+
+				POD biggestA = {15, 1, 0.6};
+				POD biggestB = {1, 10, 0.5};
+				POD biggestC = {14, 1, 1.6};
+
+				source[&biggestA] = biggestA;
+				source[&biggestB] = biggestB;
+				source[&biggestC] = biggestC;
+
+				sorted_pods s(source);
+				s.set_order(&sort_by_a, true);
+
+				Assert::IsTrue(s.at(0) == make_pod(biggestA));
+
+				s.set_order(sort_by_b(), true);
+				Assert::IsTrue(s.at(0) == make_pod(biggestB));
+
+				s.set_order(&sort_by_c, true);
+				Assert::IsTrue(s.at(0) == make_pod(biggestC));
+			}
+
+			[TestMethod]
+			void OrderedViewCanSwitchDirection()
+			{
+				pod_map source;
+
+				POD biggestA = {15, 1, 0.6};
+				POD biggestB = {1, 10, 0.5};
+				POD biggestC = {14, 1, 1.6};
+
+				source[&biggestA] = biggestA;
+				source[&biggestB] = biggestB;
+				source[&biggestC] = biggestC;
+
+				sorted_pods s(source);
+
+				s.set_order(&sort_by_a, true);
+				Assert::IsTrue(s.at(0) == make_pod(biggestA));
+
+				s.set_order(&sort_by_a, false);
+				Assert::IsTrue(s.at(s.size()-1) == make_pod(biggestA));
+
+				s.set_order(sort_by_b(), true);
+				Assert::IsTrue(s.at(0) == make_pod(biggestB));
+
+				s.set_order(sort_by_b(), false);
+				Assert::IsTrue(s.at(s.size()-1) == make_pod(biggestB));
+			}
+
+			[TestMethod]
+			void OrderedViewCanFindByKeyWithoutAnyPredicateSet()
+			{
+				pod_map source;
+
+				POD one = {-11, 21, 0.6};
+				POD two = {1, -10, 0.5};
+				POD three = {114, 1, 1.6};
+
+				source[&one] = one;
+				source[&two] = two;
+
+				sorted_pods s(source);
+
+				Assert::IsTrue(s.find_by_key(&one) != sorted_pods::npos);
+				Assert::IsTrue(s.find_by_key(&two) != sorted_pods::npos);
+				Assert::IsTrue(s.find_by_key(&three) == sorted_pods::npos);
+			}
+
+			[TestMethod]
+			void OrderedViewCanFindByKeyWithPredicateSet()
+			{
+				pod_map source;
+
+				POD one = {114, 21, 99.6};
+				POD two = {1, -10, 1.0};
+				POD three = {-11, 1, 0.006};
+				POD four = {64, 1, 1.6};
+
+				source[&one] = one;
+				source[&two] = two;
+				source[&three] = three;
+
+				sorted_pods s(source);
+
+				s.set_order(&sort_by_c, true);
+
+				Assert::IsTrue(s.find_by_key(&one) == 0);
+				Assert::IsTrue(s.find_by_key(&two) == 1);
+				Assert::IsTrue(s.find_by_key(&three) == 2);
+				Assert::IsTrue(s.find_by_key(&four) == sorted_pods::npos);
+			}
+
+			[TestMethod]
+			void OrderedViewCanFindByKeyWhenOrderChanged()
+			{
+				pod_map source;
+
+				POD one = {114, -21, 99.6};
+				POD two = {1, 0, 1.0};
+				POD three = {-11, -10, 0.006};
+				POD four = {64, 1, 1.6};
+
+				source[&one] = one;
+				source[&two] = two;
+				source[&three] = three;
+
+				sorted_pods s(source);
+
+				s.set_order(&sort_by_c, true);
+
+				Assert::IsTrue(s.find_by_key(&one) == 0);
+				Assert::IsTrue(s.find_by_key(&two) == 1);
+				Assert::IsTrue(s.find_by_key(&three) == 2);
+				Assert::IsTrue(s.find_by_key(&four) == sorted_pods::npos);
+
+				s.set_order(&sort_by_c, false); // change direction
+
+				Assert::IsTrue(s.find_by_key(&one) == 2);
+				Assert::IsTrue(s.find_by_key(&two) == 1);
+				Assert::IsTrue(s.find_by_key(&three) == 0);
+				Assert::IsTrue(s.find_by_key(&four) == sorted_pods::npos);
+
+				s.set_order(sort_by_b(), true);
+
+				Assert::IsTrue(s.find_by_key(&one) == 2);
+				Assert::IsTrue(s.find_by_key(&two) == 0);
+				Assert::IsTrue(s.find_by_key(&three) == 1);
+				Assert::IsTrue(s.find_by_key(&four) == sorted_pods::npos);
+			}
+
+			[TestMethod]
+			void OrderedViewCanFetchMapChanges()
+			{
+				pod_map source;
+
+				POD one = {114, -21, 99.6};
+				POD two = {1, 0, 1.0};
+				POD three = {-11, -10, 0.006};
+				POD four = {64, 1, 1.6};
+
+				source[&one] = one;
+				source[&two] = two;
+				source[&three] = three;
+
+				size_t initial_size = source.size();
+				sorted_pods s(source);
+				Assert::IsTrue(s.size() == source.size());
+				// Alter source map
+				source[&four] = four;
+				s.resort();
+				Assert::IsTrue(s.find_by_key(&four) != sorted_pods::npos);
+				Assert::IsTrue(initial_size + 1 == source.size());
+				Assert::IsTrue(s.size() == source.size());
+			}
+
+			[TestMethod]
+			void OrderedViewPreservesOrderAfterResort()
+			{
+				pod_map source;
+
+				POD one = {114, -21, 99.6};
+				POD two = {1, 0, 1.0};
+				POD three = {-11, -10, 0.006};
+				POD four = {64, 1, 1.6};
+
+				source[&one] = one;
+				source[&two] = two;
+				source[&three] = three;
+
+				sorted_pods s(source);
+				s.set_order(&sort_by_a, true);
+				// Check if order is valid
+				Assert::IsTrue(s.at(0) == make_pod(one));
+				Assert::IsTrue(s.at(1) == make_pod(two));
+				Assert::IsTrue(s.at(2) == make_pod(three));
+				Assert::IsTrue(s.find_by_key(&two) != sorted_pods::npos);
+				Assert::IsTrue(s.find_by_key(&four) == sorted_pods::npos);
+				// Resort(aka repopulate)
+				s.resort();
+				// Check if order is still valid
+				Assert::IsTrue(s.at(0) == make_pod(one));
+				Assert::IsTrue(s.at(1) == make_pod(two));
+				Assert::IsTrue(s.at(2) == make_pod(three));
+				Assert::IsTrue(s.find_by_key(&two) != sorted_pods::npos);
+				Assert::IsTrue(s.find_by_key(&four) == sorted_pods::npos);
+				// Alter source map and resort
+				source[&four] = four;
+				s.resort();
+				// Check if order is valid and new item there
+				Assert::IsTrue(s.at(0) == make_pod(one));
+				Assert::IsTrue(s.at(1) == make_pod(four));
+				Assert::IsTrue(s.at(2) == make_pod(two));
+				Assert::IsTrue(s.at(3) == make_pod(three));
+				Assert::IsTrue(s.find_by_key(&two) != sorted_pods::npos);
+				Assert::IsTrue(s.find_by_key(&four) != sorted_pods::npos);
+			}
+
+			[TestMethod]
+			void OrderedViewCanResortAfterOrderCahnge()
+			{
+				pod_map source;
+
+				POD one = {114, -21, 99.6};
+				POD two = {1, 0, 1.0};
+				POD three = {-11, -10, 0.006};
+				POD four = {64, 1, 1.6};
+
+				source[&one] = one;
+				source[&two] = two;
+
+				sorted_pods s(source);
+				s.set_order(&sort_by_a, true);
+				// Check if order is valid
+				Assert::IsTrue(s.at(0) == make_pod(one));
+				Assert::IsTrue(s.at(1) == make_pod(two));
+				// Change order
+				s.set_order(&sort_by_c, false);
+				// Check if order is changed
+				Assert::IsTrue(s.at(0) == make_pod(two));
+				Assert::IsTrue(s.at(1) == make_pod(one));
+				// Alter source map and resort
+				source[&three] = three;
+				source[&four] = four;
+				s.resort();
+				// Check if order is valid and new item there
+				Assert::IsTrue(s.at(0) == make_pod(three));
+				Assert::IsTrue(s.at(1) == make_pod(two));
+				Assert::IsTrue(s.at(2) == make_pod(four));
+				Assert::IsTrue(s.at(3) == make_pod(one));
+				Assert::IsTrue(s.find_by_key(&two) != sorted_pods::npos);
+				Assert::IsTrue(s.find_by_key(&four) != sorted_pods::npos);
+			}
+
+			[TestMethod]
+			void OrderedViewCanCahngeOrderAfterResort()
+			{
+				pod_map source;
+
+				POD one = {114, -21, 99.6};
+				POD two = {1, 0, 1.0};
+				POD three = {-11, -10, 0.006};
+				POD four = {64, 1, 1.6};
+
+				source[&one] = one;
+				source[&two] = two;
+
+				sorted_pods s(source);
+				s.set_order(&sort_by_a, true);
+				// Check if order is valid
+				Assert::IsTrue(s.at(0) == make_pod(one));
+				Assert::IsTrue(s.at(1) == make_pod(two));
+				// Alter source map and resort
+				source[&three] = three;
+				source[&four] = four;
+				s.resort();
+				// Set another order
+				s.set_order(&sort_by_c, false);
+				// Check if order is valid and new item there
+				Assert::IsTrue(s.at(0) == make_pod(three));
+				Assert::IsTrue(s.at(1) == make_pod(two));
+				Assert::IsTrue(s.at(2) == make_pod(four));
+				Assert::IsTrue(s.at(3) == make_pod(one));
+				Assert::IsTrue(s.find_by_key(&two) != sorted_pods::npos);
+				Assert::IsTrue(s.find_by_key(&four) != sorted_pods::npos);
+			}
+
+			[TestMethod]
+			void OrderedViewCanFetchChangesFromEmptyMap()
+			{
+				pod_map source;
+				sorted_pods s(source);
+				s.set_order(sort_by_b(), false);
+
+				Assert::IsTrue(s.size() == 0);
+
+				POD one = {114, 21, 99.6};
+				POD two = {1, 0, 11.0};
+				POD three = {-11, -10, 0.006};
+				POD four = {64, -551, 11123.6};
+				// Add couple and check they are in rigth places
+				source[&one] = one;
+				source[&two] = two;
+				s.resort();
+				Assert::IsTrue(s.size() == source.size());
+				Assert::IsTrue(s.at(0) == make_pod(two));
+				Assert::IsTrue(s.at(1) == make_pod(one));
+				// Add another couple and check they ALL are in rigth places
+				source[&three] = three;
+				source[&four] = four;
+				s.resort();
+				Assert::IsTrue(s.size() == source.size());
+				Assert::IsTrue(s.at(0) == make_pod(four));
+				Assert::IsTrue(s.at(1) == make_pod(three));
+				Assert::IsTrue(s.at(2) == make_pod(two));
+				Assert::IsTrue(s.at(3) == make_pod(one));
+
+			}
+
+			[TestMethod]
+			void OrderedViewThrowsOutOfRange()
+			{
+				pod_map source;
+
+				POD one = {114, 21, 99.6};
+				POD two = {1, 0, 11.0};
+
+				source[&one] = one;
+				source[&two] = two;
+
+				sorted_pods s(source);
+				s.set_order(sort_by_b(), false);
+
+				Assert::IsTrue(s.at(0) == make_pod(two));
+				Assert::IsTrue(s.at(1) == make_pod(one));
+				try
+				{
+					s.at(2);
+					Assert::IsTrue(false); // unexpected to be here
+				}
+				catch(std::out_of_range&)
+				{
+					Assert::IsTrue(true); // ok, we got an exception
+				}
+
+			}
+		}; // OrderedViewTests
+
+	} // namespace tests
+}
---
+++ branches/children.statistics/micro-profiler/frontend/ordered_view.h
@@ -0,0 +1,155 @@
+//	Copyright (C) 2011 by Artem A. Gevorkyan (gevorkyan.org) and Denis Burenko
+//
+//	Permission is hereby granted, free of charge, to any person obtaining a copy
+//	of this software and associated documentation files (the "Software"), to deal
+//	in the Software without restriction, including without limitation the rights
+//	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//	copies of the Software, and to permit persons to whom the Software is
+//	furnished to do so, subject to the following conditions:
+//
+//	The above copyright notice and this permission notice shall be included in
+//	all copies or substantial portions of the Software.
+//
+//	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//	THE SOFTWARE.
+
+#pragma once
+
+#include <memory>
+#include <vector>
+#include <algorithm>
+
+template <class Map>
+class ordered_view
+{
+	typedef typename Map::const_iterator stored_type;
+	typedef std::vector<typename stored_type> ordered_storage;
+
+	struct sorter;
+
+	template <typename Predicate>
+	class sorter_impl;
+
+	const Map &_map;
+	ordered_storage _ordered_data;
+	std::auto_ptr<sorter> _sorter;
+
+	// Repopulate internal storage with data from source map, ignores any predicate set.
+	void fetch_data();
+
+	const ordered_view &operator =(const ordered_view &);
+
+public:
+	typedef typename Map::value_type value_type;
+	typedef typename Map::key_type key_type;
+
+	static const size_t npos = static_cast<size_t>(-1);
+
+public:
+	ordered_view(const Map &map);
+
+	// Set order for internal orderd storage, save it until new order is set.
+	template <typename Predicate>
+	void set_order(Predicate predicate, bool ascending);
+
+	// Repopulate internal ordered storage from source map with respect to predicate set.
+	void resort();
+
+	size_t size() const;
+	const value_type &at(size_t index) const;
+	size_t find_by_key(const key_type &key) const;
+};
+
+// Sorter interface.
+// We need this to be able to switch sorter. Since concrete sorter type 
+// depends on functor type, we can't switch it without interface.
+template <class Map>
+struct ordered_view<Map>::sorter
+{
+	typedef typename std::vector<typename Map::const_iterator>::iterator target_iterator;
+
+	virtual ~sorter()	{	}
+
+	virtual void sort(target_iterator begin, target_iterator end) const = 0;
+};
+
+// Sorter implementation.
+// This stuff(i.e. desc_functor, sorter, sorter_impl) was introduced to let compiler inline functor.
+// We sacrifice one virtual call, but get bunch of functor inlines.
+template <class Map>
+template <typename Predicate>
+class ordered_view<Map>::sorter_impl : public ordered_view<Map>::sorter
+{
+	Predicate _predicate;
+	bool _ascending;
+
+public:
+	sorter_impl(Predicate predicate, bool ascending)
+		: _predicate(predicate), _ascending(ascending)
+	{	}
+
+	virtual void sort(typename sorter::target_iterator begin, typename sorter::target_iterator end) const
+	{	std::sort(begin, end, *this);	}
+
+	bool operator ()(const typename Map::const_iterator &lhs, const typename Map::const_iterator &rhs) const
+	{	return _ascending ? _predicate(lhs->first, lhs->second, rhs->first, rhs->second) : _predicate(rhs->first, rhs->second, lhs->first, lhs->second);	}
+};
+
+/// ordered_view implementation
+template <class Map>
+inline ordered_view<Map>::ordered_view(const Map &map)
+	: _map(map)
+{	fetch_data();	}
+
+template <class Map>
+inline size_t ordered_view<Map>::size() const
+{	return _ordered_data.size();	}
+
+template <class Map>
+template <typename Predicate>
+inline void ordered_view<Map>::set_order(Predicate predicate, bool ascending)
+{
+	_sorter.reset(new sorter_impl<Predicate>(predicate, ascending));
+	_sorter->sort(_ordered_data.begin(), _ordered_data.end());
+}
+
+template <class Map>
+inline void ordered_view<Map>::resort()
+{
+	fetch_data();
+
+	if (_sorter.get())
+		_sorter->sort(_ordered_data.begin(), _ordered_data.end());
+}
+
+template <class Map>
+inline void ordered_view<Map>::fetch_data()
+{
+	stored_type it = _map.begin();
+	const stored_type end = _map.end();
+
+	_ordered_data.clear();
+	_ordered_data.reserve(_map.size());
+	for(; it != end; ++it)
+		_ordered_data.push_back(it);
+}
+
+template <class Map>
+inline const typename ordered_view<Map>::value_type &ordered_view<Map>::at( size_t index ) const
+{	return *_ordered_data.at(index);	}
+
+template <class Map>
+inline size_t ordered_view<Map>::find_by_key(const key_type &key) const
+{
+	for(size_t i = 0; i < _ordered_data.size(); ++i)
+	{
+		if((*_ordered_data[i]).first == key)
+			return i;
+	}
+	return npos;
+}

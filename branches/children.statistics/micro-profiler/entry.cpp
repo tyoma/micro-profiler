ecc161276d04f49765abb2f096869caa13d25135
artem.gevorkyan@gmail.com <artem.gevorkyan@gmail.com@1676f2fc-95af-a39c-d699-6f41337c5309>
2011-09-13 13:20:57 +0000

    - unnecessary function and inclusions are removed.

--- branches/children.statistics/micro-profiler/entry.cpp
+++ branches/children.statistics/micro-profiler/entry.cpp
@@ -26,9 +26,7 @@
 
 #include <wpl/mt/thread.h>
 #include <atlbase.h>
-#include <process.h>
 #include <vector>
-#include <algorithm>
 
 namespace std
 {
@@ -77,12 +75,6 @@ namespace micro_profiler
 	namespace
 	{
 		const __int64 c_ticks_resolution(timestamp_precision());
-		FunctionStatistics fs2FS(const pair<void *, function_statistics> &s)
-		{
-			FunctionStatistics result = { reinterpret_cast<hyper>(s.first) - 5, s.second.times_called, s.second.max_reentrance, s.second.exclusive_time, s.second.inclusive_time, s.second.max_call_time };
-
-			return result;
-		}
 	}
 
 	void __declspec(dllexport) create_local_frontend(IProfilerFrontend **frontend)

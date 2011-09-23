83d8762a101f7b3113b023ce73d01e1a191dba41
burenkod <burenkod@1676f2fc-95af-a39c-d699-6f41337c5309>
2011-09-23 16:18:25 +0000

    Add initial version function_list class - listview::model implementation. Sorting and invalidated notifications are not implemented yet.

---
+++ trunk/micro-profiler.tests/frontend/FunctionListTests.cpp
@@ -0,0 +1,245 @@
+#include <frontend/function_list.h>
+#include <com_helpers.h>
+
+#include <functional>
+#include <list>
+#include <utility>
+#include <string>
+#include <sstream>
+#include <cmath>
+
+namespace std 
+{
+	using namespace std::tr1;
+	using namespace std::tr1::placeholders;
+}
+
+using namespace wpl::ui;
+using namespace micro_profiler;
+using namespace Microsoft::VisualStudio::TestTools::UnitTesting;
+
+namespace 
+{
+	using namespace std;
+
+	__int64 test_ticks_resolution = 1;
+
+	template <typename T>
+	wstring to_string(const T &value)
+	{
+		wstringstream s;
+		s << value;
+		return s.str();
+	}
+
+	void assert_row(
+		const functions_list& fl, 
+		size_t row, 
+		const wchar_t* name, 
+		const wchar_t* times_called, 
+		const wchar_t* inclusive_time, 
+		const wchar_t* exclusive_time, 
+		const wchar_t* avg_inclusive_time, 
+		const wchar_t* avg_exclusive_time, 
+		const wchar_t* max_reentrance)
+	{
+		std::wstring result;
+		result.reserve(1000);
+		fl.get_text(row, 0, result); // row number
+		Assert::IsTrue(result == to_string(row));
+		fl.get_text(row, 1, result); // name
+		Assert::IsTrue(result == name);
+		fl.get_text(row, 2, result); // times called
+		Assert::IsTrue(result == times_called);
+		fl.get_text(row, 3, result); // exclusive time
+		Assert::IsTrue(result == exclusive_time);
+		fl.get_text(row, 4, result); // inclusive time
+		Assert::IsTrue(result == inclusive_time);
+		fl.get_text(row, 5, result); // avg. exclusive time
+		Assert::IsTrue(result == avg_exclusive_time);
+		fl.get_text(row, 6, result); // avg. inclusive time
+		Assert::IsTrue(result == avg_inclusive_time);
+		fl.get_text(row, 7, result); // max reentrance
+		Assert::IsTrue(result == max_reentrance);
+	}
+
+	class myhandler
+	{
+	public:
+		typedef listview::model::index_type index_type;
+		typedef std::list<index_type> counter;
+
+		void bind2(listview::model &to) { _connection = to.invalidated += bind(&myhandler::handle, this, _1); }
+
+		size_t times() const { _counter.size(); }
+
+		counter::const_iterator begin() const { return _counter.begin(); }
+		counter::const_iterator end() const { return _counter.end(); }
+	private:
+		void handle(index_type count)
+		{
+			_counter.push_back(count);
+		}
+		
+		wpl::slot_connection _connection;
+		counter _counter;
+	};
+
+	class sri : public symbol_resolver_itf
+	{
+	public:
+		virtual std::wstring symbol_name_by_va(const void *address)
+		{
+			return to_string(address);
+		}
+
+	};
+
+}
+
+
+namespace micro_profiler
+{
+	namespace tests
+	{
+		[TestClass]
+		public ref class FunctinoListTests
+		{
+		public: 
+
+			[TestMethod]
+			void CanCreateEmptyFunctinoList()
+			{
+				sri resolver;
+				functions_list fl(test_ticks_resolution, resolver);
+
+				Assert::IsTrue(fl.get_count() == 0);
+			}
+
+			[TestMethod]
+			void FunctinoListAcceptsUpdates()
+			{
+				// INIT
+				function_statistics_detailed s1, s2;
+				FunctionStatisticsDetailed ms1, ms2;
+				std::vector<FunctionStatistics> dummy_children_buffer;
+
+				s1.times_called = 19;
+				s1.max_reentrance = 0;
+				s1.inclusive_time = 31;
+				s1.exclusive_time = 29;
+
+				s2.times_called = 10;
+				s2.max_reentrance = 3;
+				s2.inclusive_time = 7;
+				s2.exclusive_time = 5;
+
+				copy(std::make_pair((void *)1123, s1), ms1, dummy_children_buffer);
+				copy(std::make_pair((void *)2234, s2), ms2, dummy_children_buffer);
+
+				FunctionStatisticsDetailed data[] = {ms1, ms2};
+				
+				// ACT
+				sri resolver;
+				functions_list fl(test_ticks_resolution, resolver);
+				fl.update(data, 2);
+
+				// ASSERT
+				Assert::IsTrue(fl.get_count() == 2);
+			}
+
+			[TestMethod]
+			void FunctinoListCanBeClearedAndUsedAgain()
+			{
+				// INIT
+				function_statistics_detailed s1;
+				FunctionStatisticsDetailed ms1;
+				std::vector<FunctionStatistics> dummy_children_buffer;
+
+				s1.times_called = 19;
+				s1.max_reentrance = 0;
+				s1.inclusive_time = 31;
+				s1.exclusive_time = 29;
+
+				copy(std::make_pair((void *)1123, s1), ms1, dummy_children_buffer);
+
+				// ACT & ASSERT
+				sri resolver;
+				functions_list fl(test_ticks_resolution, resolver);
+				fl.update(&ms1, 1);
+
+				Assert::IsTrue(fl.get_count() == 1);
+
+				// ACT & ASSERT
+				fl.clear();
+				Assert::IsTrue(fl.get_count() == 0);
+
+				// ACT & ASSERT
+				fl.update(&ms1, 1);
+
+				Assert::IsTrue(fl.get_count() == 1);
+			}
+			
+			[TestMethod]
+			void FunctinoListCollectsUpdates()
+			{
+				// INIT
+				function_statistics_detailed s1, s2, s3, s4;
+				FunctionStatisticsDetailed ms1, ms2, ms3, ms4;
+				std::vector<FunctionStatistics> dummy_children_buffer;
+
+				s1.times_called = 19;
+				s1.max_reentrance = 0;
+				s1.inclusive_time = 31;
+				s1.exclusive_time = 29;
+
+				s2.times_called = 10;
+				s2.max_reentrance = 3;
+				s2.inclusive_time = 7;
+				s2.exclusive_time = 5;
+
+				s3.times_called = 5;
+				s3.max_reentrance = 0;
+				s3.inclusive_time = 10;
+				s3.exclusive_time = 7;
+
+				s4.times_called = 15;
+				s4.max_reentrance = 1024;
+				s4.inclusive_time = 1011;
+				s4.exclusive_time = 723;
+
+				copy(std::make_pair((void *)1123, s1), ms1, dummy_children_buffer);
+				copy(std::make_pair((void *)2234, s2), ms2, dummy_children_buffer);
+				copy(std::make_pair((void *)1123, s3), ms3, dummy_children_buffer);
+				copy(std::make_pair((void *)5555, s4), ms4, dummy_children_buffer);
+
+				FunctionStatisticsDetailed data1[] = {ms1, ms2};
+				FunctionStatisticsDetailed data2[] = {ms3, ms4};
+
+				sri resolver;
+				functions_list fl(test_ticks_resolution, resolver);
+
+				// ACT & ASSERT
+				fl.update(data1, 2);
+				Assert::IsTrue(fl.get_count() == 2);
+		
+				/* name, times_called, inclusive_time, exclusive_time, avg_inclusive_time, avg_exclusive_time, max_reentrance */
+
+				assert_row(fl, 0, L"0000045E", L"19", L"31s", L"29s", L"1.63s", L"1.53s", L"0");
+				assert_row(fl, 1, L"000008B5", L"10", L"7s", L"5s", L"700ms", L"500ms", L"3");
+
+				// ACT & ASSERT
+				fl.update(data2, 2);
+				Assert::IsTrue(fl.get_count() == 3);
+				assert_row(fl, 0, L"0000045E", L"24", L"41s", L"36s", L"1.71s", L"1.5s", L"0");
+				assert_row(fl, 1, L"000008B5", L"10", L"7s", L"5s", L"700ms", L"500ms", L"3");
+				assert_row(fl, 2, L"000015AE", L"15", L"1011s", L"723s", L"67.4s", L"48.2s", L"1024");
+			}
+
+			//myhandler h;
+			//h.bind2(fl);
+
+		};
+
+	} // namespace tests
+} // namespace micro_profiler
---
+++ trunk/micro-profiler/frontend/function_list.h
@@ -0,0 +1,60 @@
+//	Copyright (C) 2011 by Artem A. Gevorkyan (gevorkyan.org) and Denis Burenko
+//
+//	Permission is hereby granted, free of charge, to any person obtaining a copy
+//	of this software and associated documentation files (the "Software"), to deal
+//	in the Software without restriction, including without limitation the rights
+//	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//	copies of the Software, and to permit persons to whom the Software is
+//	furnished to do so, subject to the following conditions:
+//
+//	The above copyright notice and this permission notice shall be included in
+//	all copies or substantial portions of the Software.
+//
+//	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//	THE SOFTWARE.
+
+#pragma once
+
+#include "../_generated/microprofilerfrontend_i.h"
+#include "../primitives.h"
+
+#include "ordered_view.h"
+
+#include <wpl/base/concepts.h>
+#include <wpl/ui/win32/controls.h>
+
+#include <string>
+
+struct symbol_resolver_itf
+{
+	virtual ~symbol_resolver_itf() {}
+	virtual std::wstring symbol_name_by_va(const void *address) = 0;
+};
+
+
+class functions_list : public wpl::ui::listview::model, wpl::noncopyable
+{
+	micro_profiler::statistics_map _statistics;
+	ordered_view<micro_profiler::statistics_map> _view;
+
+	__int64 _ticks_resolution;
+	symbol_resolver_itf& _resolver;
+public:
+	functions_list(__int64 ticks_resolution, symbol_resolver_itf& resolver);
+	~functions_list();
+
+	void clear();
+	void update(const FunctionStatisticsDetailed *data, unsigned int count);
+
+	virtual index_type get_count() const throw();
+	virtual void get_text(index_type item, index_type subitem, std::wstring &text) const;
+	virtual void set_order(index_type column, bool ascending);
+
+	index_type get_index(const void *address) const;
+
+};

2090cf9d75832f32e411b60042a49f00287cabdf
artem.gevorkyan@gmail.com <artem.gevorkyan@gmail.com@1676f2fc-95af-a39c-d699-6f41337c5309>
2011-09-28 08:36:40 +0000

    - zipping batch file now can archive on x64 from within x86 cmd.

--- branches/children.statistics/deployment/mkzip.cmd
+++ branches/children.statistics/deployment/mkzip.cmd
@@ -1,7 +1,36 @@
 if exist "%ProgramFiles%\7-Zip\7z.exe" (
-	"%ProgramFiles%\7-Zip\7z.exe" a -tzip "%2" "%1"
-) else (
-	if exist "%ProgramFiles%\WinRar\rar.exe" (
-		"%ProgramFiles%\WinRar\rar.exe" a -ep1 "%2" "%1"
-	)
+	set arcihver="%ProgramFiles%\7-Zip\7z.exe"
+	goto use7z
 )
+if exist "%ProgramFiles(x86)%\7-Zip\7z.exe" (
+	set arcihver="%ProgramFiles(x86)%\7-Zip\7z.exe"
+	goto use7z
+)
+if exist "%ProgramW6432%\7-Zip\7z.exe" (
+	set arcihver="%ProgramW6432%\7-Zip\7z.exe"
+	goto use7z
+)
+if exist "%ProgramFiles%\WinRar\rar.exe" (
+	set arcihver="%ProgramFiles%\WinRar\rar.exe"
+	goto userar
+)
+if exist "%ProgramFiles(x86)%\WinRar\rar.exe" (
+	set arcihver="%ProgramFiles(x86)%\WinRar\rar.exe"
+	goto userar
+)
+if exist "%ProgramW6432%\WinRar\rar.exe" (
+	set arcihver="%ProgramW6432%\WinRar\rar.exe"
+	goto userar
+)
+goto noarc
+
+:use7z
+%arcihver% a -tzip "%2" "%1"
+goto exit
+
+:userar
+%arcihver% a -ep1 "%2" "%1"
+goto exit
+
+:noarc
+:exit

b3db894b3eb5af16cc918e8aa1d489836858e778
artem.gevorkyan@gmail.com <artem.gevorkyan@gmail.com@1676f2fc-95af-a39c-d699-6f41337c5309>
2011-11-28 12:58:02 +0000

    - changes from trunk reintegrated (restructuring sources: addin & module).

--- branches/children.statistics/micro-profiler/module/ProxyBridge.h
+++ branches/children.statistics/micro-profiler/module/ProxyBridge.h
@@ -27,6 +27,4 @@ extern "C"
 	BOOL WINAPI PrxDllMain(HINSTANCE hInstance, DWORD dwReason, LPVOID lpReserved);
 	STDAPI PrxDllCanUnloadNow();
 	STDAPI PrxDllGetClassObject(REFCLSID rclsid, REFIID riid, LPVOID *ppv);
-	STDAPI PrxDllRegisterServer();
-	STDAPI PrxDllUnregisterServer();
 }

392ae2a39c9db98096703aaaa8a805488fad6827
artem.gevorkyan@gmail.com <artem.gevorkyan@gmail.com@1676f2fc-95af-a39c-d699-6f41337c5309>
2011-11-30 12:14:15 +0000

    - com_helpers: utility functor class children_count_accumulator moved to function body; - com_helpers: function_statistics assigment-add operator added; - functions_list moved to micro_profiler namespace.

--- trunk/micro-profiler/frontend/ProfilerMainDialog.cpp
+++ trunk/micro-profiler/frontend/ProfilerMainDialog.cpp
@@ -33,99 +33,102 @@ using namespace std;
 using namespace wpl;
 using namespace wpl::ui;
 
-ProfilerMainDialog::ProfilerMainDialog(std::shared_ptr<functions_list> s)
-	: _statistics(s)
+namespace micro_profiler
 {
-	Create(NULL, 0);
-
-	_statistics_lv->add_column(L"#", listview::dir_none);
-	_statistics_lv->add_column(L"Function", listview::dir_ascending);
-	_statistics_lv->add_column(L"Times Called", listview::dir_descending);
-	_statistics_lv->add_column(L"Exclusive Time", listview::dir_descending);
-	_statistics_lv->add_column(L"Inclusive Time", listview::dir_descending);
-	_statistics_lv->add_column(L"Average Call Time (Exclusive:)", listview::dir_descending);
-	_statistics_lv->add_column(L"Average Call Time (Inclusive)", listview::dir_descending);
-	_statistics_lv->add_column(L"Max Recursion", listview::dir_descending);
-	_statistics_lv->add_column(L"Max Call Time", listview::dir_descending);
-
-	_statistics_lv->set_model(_statistics);
-
-	_statistics_lv->adjust_column_widths();
-}
-
-ProfilerMainDialog::~ProfilerMainDialog()
-{
-	if (IsWindow())
-		DestroyWindow();
-}
-
-LRESULT ProfilerMainDialog::OnInitDialog(UINT /*message*/, WPARAM /*wparam*/, LPARAM /*lparam*/, BOOL& handled)
-{
-	CRect clientRect;
-
-	_statistics_view = GetDlgItem(IDC_FUNCTIONS_STATISTICS);
-	_statistics_lv = wrap_listview((HWND)_statistics_view);
-	ListView_SetExtendedListViewStyle(_statistics_view, LVS_EX_FULLROWSELECT | LVS_EX_DOUBLEBUFFER | ListView_GetExtendedListViewStyle(_statistics_view));
-
-	_clear_button = GetDlgItem(IDC_BTN_CLEAR);
-	_copy_all_button = GetDlgItem(IDC_BTN_COPY_ALL);
-
-	GetClientRect(&clientRect);
-	RelocateControls(clientRect.Size());
-
-	::EnableMenuItem(GetSystemMenu(FALSE), SC_CLOSE, MF_BYCOMMAND | MF_DISABLED | MF_GRAYED);
-	SetIcon(::LoadIcon(g_instance, MAKEINTRESOURCE(IDI_APPMAIN)), TRUE);
-	handled = TRUE;
-	return 1;	// Let the system set the focus
-}
-
-LRESULT ProfilerMainDialog::OnSize(UINT /*message*/, WPARAM /*wparam*/, LPARAM lparam, BOOL &handled)
-{
-	RelocateControls(CSize(LOWORD(lparam), HIWORD(lparam)));
-	handled = TRUE;
-	return 1;
-}
-
-LRESULT ProfilerMainDialog::OnClearStatistics(WORD /*code*/, WORD /*control_id*/, HWND /*control*/, BOOL &handled)
-{
-	_statistics->clear();
-	handled = TRUE;
-	return 0;
-}
-
-LRESULT ProfilerMainDialog::OnCopyAll(WORD /*code*/, WORD /*control_id*/, HWND /*control*/, BOOL &handled)
-{
-	wstring result;
-
-	_statistics->print(result);
-	if (OpenClipboard())
+	ProfilerMainDialog::ProfilerMainDialog(std::shared_ptr<functions_list> s)
+		: _statistics(s)
 	{
-		if (HGLOBAL gtext = ::GlobalAlloc(GMEM_MOVEABLE, (result.size() + 1) * sizeof(wchar_t)))
-		{
-			wchar_t *gtext_memory = reinterpret_cast<wchar_t *>(::GlobalLock(gtext));
+		Create(NULL, 0);
 
-			copy(result.c_str(), result.c_str() + result.size() + 1, gtext_memory);
-			::GlobalUnlock(gtext_memory);
-			::EmptyClipboard();
-			::SetClipboardData(CF_UNICODETEXT, gtext);
-		}
-		CloseClipboard();
+		_statistics_lv->add_column(L"#", listview::dir_none);
+		_statistics_lv->add_column(L"Function", listview::dir_ascending);
+		_statistics_lv->add_column(L"Times Called", listview::dir_descending);
+		_statistics_lv->add_column(L"Exclusive Time", listview::dir_descending);
+		_statistics_lv->add_column(L"Inclusive Time", listview::dir_descending);
+		_statistics_lv->add_column(L"Average Call Time (Exclusive:)", listview::dir_descending);
+		_statistics_lv->add_column(L"Average Call Time (Inclusive)", listview::dir_descending);
+		_statistics_lv->add_column(L"Max Recursion", listview::dir_descending);
+		_statistics_lv->add_column(L"Max Call Time", listview::dir_descending);
+
+		_statistics_lv->set_model(_statistics);
+
+		_statistics_lv->adjust_column_widths();
 	}
-	handled = TRUE;
-	return 0;
-}
 
-void ProfilerMainDialog::RelocateControls(const CSize &size)
-{
-	const int spacing = 7;
-	CRect rcButton, rc(CPoint(0, 0), size);
+	ProfilerMainDialog::~ProfilerMainDialog()
+	{
+		if (IsWindow())
+			DestroyWindow();
+	}
 
-	_clear_button.GetWindowRect(rcButton);
-	rc.DeflateRect(spacing, spacing, spacing, spacing + rcButton.Height());
-	rcButton.MoveToXY(rc.left, rc.bottom);
-	_clear_button.MoveWindow(rcButton);
-	rcButton.MoveToX(rcButton.Width() + spacing);
-	_copy_all_button.MoveWindow(rcButton);
-	rc.DeflateRect(0, 0, 0, spacing);
-	_statistics_view.MoveWindow(rc);
+	LRESULT ProfilerMainDialog::OnInitDialog(UINT /*message*/, WPARAM /*wparam*/, LPARAM /*lparam*/, BOOL& handled)
+	{
+		CRect clientRect;
+
+		_statistics_view = GetDlgItem(IDC_FUNCTIONS_STATISTICS);
+		_statistics_lv = wrap_listview((HWND)_statistics_view);
+		ListView_SetExtendedListViewStyle(_statistics_view, LVS_EX_FULLROWSELECT | LVS_EX_DOUBLEBUFFER | ListView_GetExtendedListViewStyle(_statistics_view));
+
+		_clear_button = GetDlgItem(IDC_BTN_CLEAR);
+		_copy_all_button = GetDlgItem(IDC_BTN_COPY_ALL);
+
+		GetClientRect(&clientRect);
+		RelocateControls(clientRect.Size());
+
+		::EnableMenuItem(GetSystemMenu(FALSE), SC_CLOSE, MF_BYCOMMAND | MF_DISABLED | MF_GRAYED);
+		SetIcon(::LoadIcon(g_instance, MAKEINTRESOURCE(IDI_APPMAIN)), TRUE);
+		handled = TRUE;
+		return 1;	// Let the system set the focus
+	}
+
+	LRESULT ProfilerMainDialog::OnSize(UINT /*message*/, WPARAM /*wparam*/, LPARAM lparam, BOOL &handled)
+	{
+		RelocateControls(CSize(LOWORD(lparam), HIWORD(lparam)));
+		handled = TRUE;
+		return 1;
+	}
+
+	LRESULT ProfilerMainDialog::OnClearStatistics(WORD /*code*/, WORD /*control_id*/, HWND /*control*/, BOOL &handled)
+	{
+		_statistics->clear();
+		handled = TRUE;
+		return 0;
+	}
+
+	LRESULT ProfilerMainDialog::OnCopyAll(WORD /*code*/, WORD /*control_id*/, HWND /*control*/, BOOL &handled)
+	{
+		wstring result;
+
+		_statistics->print(result);
+		if (OpenClipboard())
+		{
+			if (HGLOBAL gtext = ::GlobalAlloc(GMEM_MOVEABLE, (result.size() + 1) * sizeof(wchar_t)))
+			{
+				wchar_t *gtext_memory = reinterpret_cast<wchar_t *>(::GlobalLock(gtext));
+
+				copy(result.c_str(), result.c_str() + result.size() + 1, gtext_memory);
+				::GlobalUnlock(gtext_memory);
+				::EmptyClipboard();
+				::SetClipboardData(CF_UNICODETEXT, gtext);
+			}
+			CloseClipboard();
+		}
+		handled = TRUE;
+		return 0;
+	}
+
+	void ProfilerMainDialog::RelocateControls(const CSize &size)
+	{
+		const int spacing = 7;
+		CRect rcButton, rc(CPoint(0, 0), size);
+
+		_clear_button.GetWindowRect(rcButton);
+		rc.DeflateRect(spacing, spacing, spacing, spacing + rcButton.Height());
+		rcButton.MoveToXY(rc.left, rc.bottom);
+		_clear_button.MoveWindow(rcButton);
+		rcButton.MoveToX(rcButton.Width() + spacing);
+		_copy_all_button.MoveWindow(rcButton);
+		rc.DeflateRect(0, 0, 0, spacing);
+		_statistics_view.MoveWindow(rc);
+	}
 }

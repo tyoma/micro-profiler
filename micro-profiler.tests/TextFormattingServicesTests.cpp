798ed1ce82427bfa9b7aac49f4237e0a8c5eda79
Artem Gevorkyan <artem.gevorkyan@gmail.com>
2011-12-01 21:16:24 +0000

    - time formatting function refactored.
    git-svn-id: http://micro-profiler.googlecode.com/svn/trunk@389 1676f2fc-95af-a39c-d699-6f41337c5309

--- micro-profiler.tests/TextFormattingServicesTests.cpp
+++ micro-profiler.tests/TextFormattingServicesTests.cpp
@@ -1,5 +1,7 @@
 #include <common/formatting.h>
 
+#include <limits>
+
 using namespace std;
 using namespace Microsoft::VisualStudio::TestTools::UnitTesting;
 
@@ -28,11 +30,13 @@ namespace micro_profiler
 
 
 			[TestMethod]
-			void EscapeNumbersUnderPrecision()
+			void EscapeNumbersUnderPrecisionAsZeroes()
 			{
 				// INIT / ACT / ASSERT
 				Assert::IsTrue(L"0s" == format_interval_proxy(0.00000000049));
 				Assert::IsTrue(L"0s" == format_interval_proxy(-0.00000000049));
+				Assert::IsTrue(L"0s" == format_interval_proxy(-0.00000000000049));
+				Assert::IsTrue(L"0s" == format_interval_proxy(numeric_limits<double>::min()));
 			}
 
 
@@ -74,6 +78,7 @@ namespace micro_profiler
 			void EscapeNonZeroMillisecondValues()
 			{
 				// INIT / ACT / ASSERT
+				Assert::IsTrue(L"1ms" == format_interval_proxy(0.0009995000000000001));
 				Assert::IsTrue(L"1ms" == format_interval_proxy(0.001));
 				Assert::IsTrue(L"1.01ms" == format_interval_proxy(0.00101));
 				Assert::IsTrue(L"3.79ms" == format_interval_proxy(0.00379));
@@ -92,17 +97,17 @@ namespace micro_profiler
 			void EscapeNonZeroMicrosecondValues()
 			{
 				// INIT / ACT / ASSERT
-				Assert::IsTrue(L"3us" == format_interval_proxy(0.000003));
-				Assert::IsTrue(L"2.09us" == format_interval_proxy(0.0000020949));
-				Assert::IsTrue(L"1.97us" == format_interval_proxy(0.00000197));
-				Assert::IsTrue(L"13.6us" == format_interval_proxy(0.00001364555));
-				Assert::IsTrue(L"117us" == format_interval_proxy(0.000117489));
-				Assert::IsTrue(L"999us" == format_interval_proxy(0.00099945555));
-				Assert::IsTrue(L"-7.91us" == format_interval_proxy(-0.000007914));
-				Assert::IsTrue(L"-5.09us" == format_interval_proxy(-0.000005094));
-				Assert::IsTrue(L"-17us" == format_interval_proxy(-0.000017));
-				Assert::IsTrue(L"-231us" == format_interval_proxy(-0.000231));
-				Assert::IsTrue(L"-999us" == format_interval_proxy(-0.000999));
+				Assert::IsTrue(L"3\x03bcs" == format_interval_proxy(0.000003));
+				Assert::IsTrue(L"2.09\x03bcs" == format_interval_proxy(0.0000020949));
+				Assert::IsTrue(L"1.97\x03bcs" == format_interval_proxy(0.00000197));
+				Assert::IsTrue(L"13.6\x03bcs" == format_interval_proxy(0.00001364555));
+				Assert::IsTrue(L"117\x03bcs" == format_interval_proxy(0.000117489));
+				Assert::IsTrue(L"999\x03bcs" == format_interval_proxy(0.00099945555));
+				Assert::IsTrue(L"-7.91\x03bcs" == format_interval_proxy(-0.000007914));
+				Assert::IsTrue(L"-5.09\x03bcs" == format_interval_proxy(-0.000005094));
+				Assert::IsTrue(L"-17\x03bcs" == format_interval_proxy(-0.000017));
+				Assert::IsTrue(L"-231\x03bcs" == format_interval_proxy(-0.000231));
+				Assert::IsTrue(L"-999\x03bcs" == format_interval_proxy(-0.000999));
 			}
 
 

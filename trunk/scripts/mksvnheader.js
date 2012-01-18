d50bb05ac56ccb671aa5ea99e4f646d5689ba21f
artem.gevorkyan@gmail.com <artem.gevorkyan@gmail.com@1676f2fc-95af-a39c-d699-6f41337c5309>
2012-01-18 21:52:09 +0000

    - automatic version stamping of the micro-profiler.dll

--- trunk/scripts/mksvnheader.js
+++ trunk/scripts/mksvnheader.js
@@ -1,15 +1,18 @@
 var stdin = WScript.StdIn;
 var stdout = WScript.StdOut;
 var splitter = /(.*)\: (.*)/;
+var prefix = WScript.Arguments.length > 0 ? WScript.Arguments(0) : "";
 
-while (!stdin.AtEndOfStream)	{
+while (!stdin.AtEndOfStream) {
 	var line = stdin.ReadLine();
 	var m = line.match(splitter);
 
 	if (m) {
-		var macro = "SVN_" + m[1].toUpperCase().replace(/ /g, "_");
-		var value = macro != "SVN_REVISION" && macro != "SVN_LAST_CHANGED_REV" ? "\"" + m[2] + "\"" : m[2];
+		var macro = prefix + "_" + m[1].toUpperCase().replace(/ /g, "_");
+		var value = m[2].replace(/\\/g, "\\\\");
+		var value_str = "\"" + value + "\"";
 
 		stdout.WriteLine("#define " + macro + " " + value);
+		stdout.WriteLine("#define " + macro + "_STR " + value_str);
 	}
 }

c306df0e9b60a9cc88b61034915cc2fbf33f45ae
artem.gevorkyan@gmail.com <artem.gevorkyan@gmail.com@1676f2fc-95af-a39c-d699-6f41337c5309>
2012-01-19 09:42:27 +0000

    - path with spaces supported; - repeated building of installer supported.

--- trunk/deployment/versioned-rename-template.cmd
+++ trunk/deployment/versioned-rename-template.cmd
@@ -1,1 +1,2 @@
-ren "%1" "%~n1.v<Version Major>.<Version Minor>.<Revision>%~x1"
+del "%~dp1%~n1.v<Version Major>.<Version Minor>.<Revision>%~x1"
+ren %1 "%~n1.v<Version Major>.<Version Minor>.<Revision>%~x1"

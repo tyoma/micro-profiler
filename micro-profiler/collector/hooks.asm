1898c3120bc5dce53ac2bf6647cf249f83851434
Artem Gevorkyan <artem.gevorkyan@gmail.com>
2014-02-14 13:15:48 +0000

    - x86 hook optimized slightly more.
    git-svn-id: http://micro-profiler.googlecode.com/svn/trunk@536 1676f2fc-95af-a39c-d699-6f41337c5309

--- micro-profiler/collector/hooks.asm
+++ micro-profiler/collector/hooks.asm
@@ -24,21 +24,19 @@ IF _M_IX86
 	.model flat
 	.code
 
-	extern ?track@calls_collector@micro_profiler@@QAEXABUcall_record@2@@Z:near
+	extern ?track@calls_collector@micro_profiler@@QAEXUcall_record@2@@Z:near
 	extern ?_instance@calls_collector@micro_profiler@@0V12@A:dword
 
-
-	SAVEREGS	macro
-		mov	dword ptr [esp - 10h], eax
-		mov	dword ptr [esp - 14h], ecx
-		mov	dword ptr [esp - 18h], edx
+	PUSHREGS	macro
+		push	eax
+		push	ecx
+		push	edx
 	endm
 
-	LOADREGS	macro
+	POPREGS	macro
 		pop	edx
 		pop	ecx
 		pop	eax
-		add	esp, 0Ch
 	endm
 
 	PUSHRDTSC	macro
@@ -48,27 +46,23 @@ IF _M_IX86
 	endm
 
 	_profile_enter	proc
-		SAVEREGS
-		PUSHRDTSC
-		mov	ecx, esp
-		sub	esp, 10h
+		PUSHREGS
+		mov	ecx, [esp + 0Ch]
 		push	ecx
+		PUSHRDTSC
 		mov	ecx, offset ?_instance@calls_collector@micro_profiler@@0V12@A
-		call	?track@calls_collector@micro_profiler@@QAEXABUcall_record@2@@Z
-		LOADREGS
+		call	?track@calls_collector@micro_profiler@@QAEXUcall_record@2@@Z
+		POPREGS
 		ret
 	_profile_enter	endp
 
 	_profile_exit	proc
-		SAVEREGS
+		PUSHREGS
 		push 0
 		PUSHRDTSC
-		mov	ecx, esp
-		sub	esp, 0Ch
-		push	ecx
 		mov	ecx, offset ?_instance@calls_collector@micro_profiler@@0V12@A
-		call	?track@calls_collector@micro_profiler@@QAEXABUcall_record@2@@Z
-		LOADREGS
+		call	?track@calls_collector@micro_profiler@@QAEXUcall_record@2@@Z
+		POPREGS
 		ret
 	_profile_exit	endp
 

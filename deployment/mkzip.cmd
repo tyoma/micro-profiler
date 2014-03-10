d8ae5e8e88fc32138b21ee9c91a3081a98856401
Artem Gevorkyan <artem.gevorkyan@gmail.com>
2014-03-10 07:25:13 +0000

    - maximized the demanded compression level in mkzip.cmd.
    git-svn-id: http://micro-profiler.googlecode.com/svn/trunk@563 1676f2fc-95af-a39c-d699-6f41337c5309

--- deployment/mkzip.cmd
+++ deployment/mkzip.cmd
@@ -1,3 +1,7 @@
+if exist "7z.exe" (
+	set arcihver="7z.exe"
+	goto use7z
+)
 if exist "%ProgramFiles%\7-Zip\7z.exe" (
 	set arcihver="%ProgramFiles%\7-Zip\7z.exe"
 	goto use7z
@@ -9,6 +13,10 @@ if exist "%ProgramFiles(x86)%\7-Zip\7z.exe" (
 if exist "%ProgramW6432%\7-Zip\7z.exe" (
 	set arcihver="%ProgramW6432%\7-Zip\7z.exe"
 	goto use7z
+)
+if exist "rar.exe" (
+	set arcihver="rar.exe"
+	goto userar
 )
 if exist "%ProgramFiles%\WinRar\rar.exe" (
 	set arcihver="%ProgramFiles%\WinRar\rar.exe"
@@ -25,11 +33,11 @@ if exist "%ProgramW6432%\WinRar\rar.exe" (
 goto noarc
 
 :use7z
-%arcihver% a -tzip %2 %1
+%arcihver% a -tzip -mx9 %2 %1
 goto exit
 
 :userar
-%arcihver% a -ep1 %2 %1
+%arcihver% a -ep1 -m5 %2 %1
 goto exit
 
 :noarc

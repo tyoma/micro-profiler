56ce99887318604eb2fad1547c1b2d808e581254
Artem Gevorkyan <artem.gevorkyan@gmail.com>
2014-03-10 11:25:58 +0000

    - version stamping automation is now solely built on cmd.exe (no .js scripts anymore).
    git-svn-id: http://micro-profiler.googlecode.com/svn/trunk@564 1676f2fc-95af-a39c-d699-6f41337c5309

--- deployment/wix/makeup-version.cmd
+++
@@ -1,7 +0,0 @@
-@echo off
-setlocal
-for /f "tokens=3" %%g in ('type %1 ^| findstr /L "Version Major: .*"') do set VerMajor=%%g
-for /f "tokens=3" %%g in ('type %1 ^| findstr /L "Version Minor: .*"') do set VerMinor=%%g
-for /f "tokens=2" %%g in ('svn info %2 ^| findstr /L "Revision: .*"') do set VerRevision=%%g
-echo %VerMajor%.%VerMinor%.%VerRevision%
-endlocal

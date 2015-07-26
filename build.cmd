7b89714935b2e055b058899ffb6ac2fac524233e
Artem G <artem.gevorkyan@gmail.com>
2015-07-26 10:00:44 -0600

    Autobuild Batch: Autoincrement, Commit, Checkout

---
+++ build.cmd
@@ -0,0 +1,38 @@
+rem 1. pull
+rem 2.+increment build number
+rem 3.+commit / store hash
+rem 4. push
+rem 5.+checkout at one step before stored hash
+rem 6. tag
+rem 7. build
+
+rem 2. Increment build number...
+call :incrementfield version-info.txt "Build"
+
+rem 3. Make a commit and store its hash...
+git add version-info.txt
+for /f "tokens=2 delims=[] " %%g in ('git commit -m "New build number..." ^| findstr /i "\[.*\].*"') do set commithash=%%g
+
+rem 5. Checkout a build revision (one before 'commithash')...
+git checkout %commithash%~1
+
+rem 7. Start the build...
+
+goto :end
+
+:incrementfield
+	set tmp="%~1.tmp"
+	del /q %tmp%
+	for /f "tokens=1,2 delims=:" %%i in (%1) do call :incrementfieldline "%%i" %%j %2 >> %tmp%
+	move /y %tmp% %1
+exit /b 0
+
+:incrementfieldline
+	@echo off
+	set /a value=%2
+	if %1==%3 set /a value=value+1
+	echo %~1: %value%
+exit /b 0
+
+:end
+

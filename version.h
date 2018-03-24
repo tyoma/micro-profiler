5ac10aed70c114e6af9900248c78543f1b35d12b
Artem G <artem.gevorkyan@gmail.com>
2018-03-23 20:38:46 -0600

    Product version record in .dll(s) fixed
    - correct TEXTINCLUDE specification so that VisualStudio wouldn't corrupt resources.

--- version.h
+++ version.h
@@ -1,7 +1,8 @@
 #pragma once  
-#define MAKE_VERSION_STRING(major, minor, branch, build) #major "." #minor "." #build "." #branch
 #define MP_VERSION_MAJOR 1 
 #define MP_VERSION_MINOR 2 
 #define MP_BUILD 598 
 #define MP_BRANCH 0 
+#define MAKE_TOKEN(token) #token
+#define MAKE_VERSION_STRING(major, minor, build, branch) MAKE_TOKEN(major) "." MAKE_TOKEN(minor) "." MAKE_TOKEN(build) "." MAKE_TOKEN(branch)
 #define MP_VERSION_STR MAKE_VERSION_STRING(MP_VERSION_MAJOR, MP_VERSION_MINOR, MP_BUILD, MP_BRANCH)

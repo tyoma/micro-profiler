60cf38adc73b30387fdd140c7627af7ae01d39cf
Artem G <artem.gevorkyan@gmail.com>
2018-10-10 22:24:26 -0600

    Slight refactoring of ordered_view - polyfunctions used instead their emulation via interfaces/implementations

--- frontend/ordered_view.h
+++ frontend/ordered_view.h
@@ -45,8 +45,8 @@ namespace micro_profiler
 		void detach() throw();
 
 		// Set order for internal orderd storage, save it until new order is set.
-		template <typename Predicate>
-		void set_order(Predicate predicate, bool ascending);
+		template <typename PredicateT>
+		void set_order(PredicateT predicate, bool ascending);
 
 		template <typename ExtractorT>
 		void project_value(const ExtractorT &extractor);
@@ -60,65 +60,62 @@ namespace micro_profiler
 		size_t find_by_key(const key_type &key) const;
 
 	private:
-		typedef typename Map::const_iterator stored_type;
-		typedef std::vector<typename stored_type> ordered_storage;
+		typedef std::vector<const value_type *> ordered_container;
 
-		struct extractor;
-		struct sorter;
-
-		template <typename Predicate>
-		class sorter_impl;
+		template <typename PredicateT>
+		class predicate_wrap_a;
+		template <typename PredicateT>
+		class predicate_wrap_d;
 
 	private:
 		// Repopulate internal storage with data from source map, ignores any predicate set.
 		void fetch_data();
 
+		template <typename PredicateT>
+		void sort(const PredicateT &predicate);
+
 		const ordered_view &operator =(const ordered_view &);
 
 	private:
 		const Map *_map;
-		ordered_storage _ordered_data;
-		std::auto_ptr<sorter> _sorter;
+		ordered_container _ordered_data;
+		std::function<void()> _sorter;
 		std::function<float(const typename Map::mapped_type &entry)> _extractor;
 		bool _ascending;
 	};
 
-
-	// Sorter interface.
-	// We need this to be able to switch sorter. Since concrete sorter type 
-	// depends on functor type, we can't switch it without interface.
-	template <class Map>
-	struct ordered_view<Map>::sorter
-	{
-		typedef typename std::vector<typename Map::const_iterator>::iterator target_iterator;
-
-		virtual ~sorter()	{	}
-
-		virtual void sort(target_iterator begin, target_iterator end) const = 0;
-	};
-
-
-	// Sorter implementation.
-	// This stuff(i.e. desc_functor, sorter, sorter_impl) was introduced to let compiler inline functor.
-	// We sacrifice one virtual call, but get bunch of functor inlines.
-	template <class Map>
-	template <typename Predicate>
-	class ordered_view<Map>::sorter_impl : public ordered_view<Map>::sorter
+	template <typename Map>
+	template <typename PredicateT>
+	class ordered_view<Map>::predicate_wrap_a
 	{
 	public:
-		sorter_impl(Predicate predicate, bool ascending)
-			: _predicate(predicate), _ascending(ascending)
-		{	}
+		predicate_wrap_a(const PredicateT &predicate)
+			: _predicate(predicate)
+		{ }
 
-		virtual void sort(typename sorter::target_iterator begin, typename sorter::target_iterator end) const
-		{	std::sort(begin, end, *this);	}
-
-		bool operator ()(const typename Map::const_iterator &lhs, const typename Map::const_iterator &rhs) const
-		{	return _ascending ? _predicate(lhs->first, lhs->second, rhs->first, rhs->second) : _predicate(rhs->first, rhs->second, lhs->first, lhs->second);	}
+		template <typename ArgT>
+		bool operator ()(ArgT lhs, ArgT rhs) const
+		{	return _predicate(lhs->first, lhs->second, rhs->first, rhs->second);	}
 
 	private:
-		Predicate _predicate;
-		bool _ascending;
+		PredicateT _predicate;
+	};
+
+	template <typename Map>
+	template <typename PredicateT>
+	class ordered_view<Map>::predicate_wrap_d
+	{
+	public:
+		predicate_wrap_d(const PredicateT &predicate)
+			: _predicate(predicate)
+		{ }
+
+		template <typename ArgT>
+		bool operator ()(ArgT lhs, ArgT rhs) const
+		{	return _predicate(rhs->first, rhs->second, lhs->first, lhs->second);	}
+
+	private:
+		PredicateT _predicate;
 	};
 
 
@@ -141,12 +138,15 @@ namespace micro_profiler
 	{	return _ordered_data.size();	}
 
 	template <class Map>
-	template <typename Predicate>
-	inline void ordered_view<Map>::set_order(Predicate predicate, bool ascending)
+	template <typename PredicateT>
+	inline void ordered_view<Map>::set_order(PredicateT predicate, bool ascending)
 	{
-		_sorter.reset(new sorter_impl<Predicate>(predicate, ascending));
+		if (ascending)
+			_sorter = std::bind(&ordered_view::sort< predicate_wrap_a<PredicateT> >, this, predicate_wrap_a<PredicateT>(predicate));
+		else
+			_sorter = std::bind(&ordered_view::sort< predicate_wrap_d<PredicateT> >, this, predicate_wrap_d<PredicateT>(predicate));
 		_ascending = ascending;
-		_sorter->sort(_ordered_data.begin(), _ordered_data.end());
+		_sorter();
 	}
 
 	template <class Map>
@@ -159,8 +159,8 @@ namespace micro_profiler
 	{
 		fetch_data();
 
-		if (_sorter.get())
-			_sorter->sort(_ordered_data.begin(), _ordered_data.end());
+		if (_sorter)
+			_sorter();
 	}
 
 	template <class Map>
@@ -170,9 +170,14 @@ namespace micro_profiler
 			return;
 		_ordered_data.clear();
 		_ordered_data.reserve(_map->size());
-		for(stored_type i = _map->begin(), end = _map->end(); i != end; ++i)
-			_ordered_data.push_back(i);
+		for (typename Map::const_iterator i = _map->begin(), end = _map->end(); i != end; ++i)
+			_ordered_data.push_back(&*i);
 	}
+
+	template <typename Map>
+	template <typename PredicateT>
+	inline void ordered_view<Map>::sort(const PredicateT &predicate)
+	{	std::sort(_ordered_data.begin(), _ordered_data.end(), predicate);	}
 
 	template <class Map>
 	inline const typename ordered_view<Map>::value_type &ordered_view<Map>::at(size_t index) const

cdce25647fb33cfa6ea4dd581a1750428e484b6d
Artem G <artem.gevorkyan@gmail.com>
2019-09-18 18:16:35 -0600

    FunctionSizesAreValid test is fixed to pass on Android and 64-bit Linux

--- micro-profiler.tests/guineapigs/symbol_container_2_internal.cpp
+++ micro-profiler.tests/guineapigs/symbol_container_2_internal.cpp
@@ -13,4 +13,26 @@ extern "C" void bubble_sort(int * volatile begin, int * volatile end)
 				*(j + 1) = tmp;
 			}
 		}
+	for (int *i = begin; i != end; ++i)
+		for (int *j = begin; j != end - 1; ++j)
+		{
+			if (*j < *(j + 1))
+			{
+				int tmp = *j;
+
+				*j = *(j + 1);
+				*(j + 1) = tmp;
+			}
+		}
+	for (int *i = begin; i != end; ++i)
+		for (int *j = begin; j != end - 1; ++j)
+		{
+			if (*j > *(j + 1))
+			{
+				int tmp = *j;
+
+				*j = *(j + 1);
+				*(j + 1) = tmp;
+			}
+		}
 }

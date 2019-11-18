60e84d5dd4a7bb09944237bd4ed81f3e33bb4302
Artem G <artem.gevorkyan@gmail.com>
2019-11-18 07:49:13 -0700

    cmake build-all script added
    - vsct compiled menu is now put to a proper intermediate directory, so that vs extension doesn't get re-linked.

---
+++ build.sh
@@ -0,0 +1,20 @@
+echo Building Android binaries...
+mkdir _build.android.arm
+cd _build.android.arm
+cmake .. -DCMAKE_TOOLCHAIN=/mnt/c/android/android-ndk-r20-linux/build/cmake/android.toolchain.cmake -DCMAKE_BUILD_TYPE=Release -DANDROID_STL=c++_static
+make -j4
+cd ..
+
+echo "Building Linux (x86_64) binaries..."
+mkdir _build.linux.x86_64
+cd _build.linux.x86_64
+cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS="-m64" -DCMAKE_C_FLAGS="-m64"
+make -j4
+cd ..
+
+echo "Building Linux (x86) binaries..."
+mkdir _build.linux.x86
+cd _build.linux.x86
+cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS="-m32" -DCMAKE_C_FLAGS="-m32"
+make -j4
+cd ..

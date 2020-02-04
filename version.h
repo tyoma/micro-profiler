efee84fecd8cb2044fe7672b44c5166d64cc98d8
Artem G <artem.gevorkyan@gmail.com>
2020-02-03 22:57:56 -0700

    New build number...

--- version.h
+++ version.h
@@ -1,7 +1,7 @@
 #pragma once  
 #define MP_VERSION_MAJOR 1 
 #define MP_VERSION_MINOR 5 
-#define MP_BUILD 613 
+#define MP_BUILD 614 
 #define MP_BRANCH 0 
 #define MAKE_TOKEN(token) #token 
 #define MAKE_VERSION_STRING(major, minor, build, branch) MAKE_TOKEN(major) "." MAKE_TOKEN(minor) "." MAKE_TOKEN(build) "." MAKE_TOKEN(branch)

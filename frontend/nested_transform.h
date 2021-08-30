1835cfeb75e2c7e52daa8b8ebb28c6887910328c
Artem G <artem.gevorkyan@gmail.com>
2021-08-30 10:02:36 -0600

    nested transforms (callers and callees) are implemented (to be used in flatten view for callers and callees)

---
+++ frontend/nested_transform.h
@@ -0,0 +1,99 @@
+//	Copyright (c) 2011-2021 by Artem A. Gevorkyan (gevorkyan.org)
+//
+//	Permission is hereby granted, free of charge, to any person obtaining a copy
+//	of this software and associated documentation files (the "Software"), to deal
+//	in the Software without restriction, including without limitation the rights
+//	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//	copies of the Software, and to permit persons to whom the Software is
+//	furnished to do so, subject to the following conditions:
+//
+//	The above copyright notice and this permission notice shall be included in
+//	all copies or substantial portions of the Software.
+//
+//	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//	THE SOFTWARE.
+
+#pragma once
+
+namespace micro_profiler
+{
+	template <typename U>
+	class callees_transform
+	{
+	public:
+		typedef typename U::key_type key_type;
+		typedef decltype(static_cast<typename U::mapped_type *>(0)->callees) nested_container_type;
+		typedef typename nested_container_type::const_iterator nested_iterator_type;
+
+	public:
+		callees_transform(const U &underlying)
+			: _underlying(underlying)
+		{	}
+
+		nested_iterator_type begin(const key_type &key) const
+		{
+			auto i = _underlying.find(key);
+			return i != _underlying.end() ? i->second.callees.begin() : _empty_nested.end();
+		}
+
+		nested_iterator_type end(const key_type &key) const
+		{
+			auto i = _underlying.find(key);
+			return i != _underlying.end() ? i->second.callees.end() : _empty_nested.end();
+		}
+
+		template <typename T1, typename T2>
+		static const T2 &get(const T1 &, const T2 &value)
+		{	return value;	}
+
+	private:
+		void operator =(const callees_transform &rhs);
+
+	private:
+		const U &_underlying;
+		const nested_container_type _empty_nested;
+	};
+
+
+	template <typename U>
+	class callers_transform
+	{
+	public:
+		typedef typename U::key_type key_type;
+		typedef decltype(static_cast<typename U::mapped_type *>(0)->callers) nested_container_type;
+		typedef typename nested_container_type::const_iterator nested_iterator_type;
+
+	public:
+		callers_transform(const U &underlying)
+			: _underlying(underlying)
+		{	}
+
+		nested_iterator_type begin(const key_type &key) const
+		{
+			auto i = _underlying.find(key);
+			return i != _underlying.end() ? i->second.callers.begin() : _empty_nested.end();
+		}
+
+		nested_iterator_type end(const key_type &key) const
+		{
+			auto i = _underlying.find(key);
+			return i != _underlying.end() ? i->second.callers.end() : _empty_nested.end();
+		}
+
+		template <typename T1, typename T2>
+		static const T2 &get(const T1 &, const T2 &value)
+		{	return value;	}
+
+	private:
+		void operator =(const callers_transform &rhs);
+
+	private:
+		const U &_underlying;
+		const nested_container_type _empty_nested;
+	};
+}
---
+++ frontend/tests/NestedTransformTests.cpp
@@ -0,0 +1,160 @@
+#include <frontend/nested_transform.h>
+
+#include <list>
+#include <map>
+#include <unordered_map>
+#include <ut/assert.h>
+#include <ut/test.h>
+
+using namespace std;
+
+namespace micro_profiler
+{
+	namespace tests
+	{
+		namespace
+		{
+			struct statistics1_t
+			{
+				map<int, string> callees;
+				vector<double> callers;
+			};
+
+			struct statistics2_t
+			{
+				list<int> callees;
+				unordered_map<int, int> callers;
+			};
+		}
+
+		begin_test_suite( CalleesStatisticsTransformTests )
+			typedef map<int, statistics1_t> map11_t;
+			typedef unordered_map<string, statistics1_t> map12_t;
+			typedef map<int, statistics2_t> map21_t;
+			typedef map<string, statistics2_t> map22_t;
+
+			test( NoLinkedStatisticsIsProvidedForEmptyCallees )
+			{
+				// INIT
+				map11_t m11;
+				map12_t m12;
+				map21_t m21;
+				map22_t m22;
+
+				m11[314];
+				m12["abc"];
+				m21[273];
+				m22["lorem"];
+
+				// INIT / ACT
+				callees_transform<map11_t> t11(m11);
+				callees_transform<map12_t> t12(m12);
+				callees_transform<map21_t> t21(m21);
+				callees_transform<map22_t> t22(m22);
+
+				// ACT / ASSERT
+				assert_equal(t11.end(314), t11.begin(314));
+				assert_equal(t12.end("abc"), t12.begin("abc"));
+				assert_equal(t21.end(273), t21.begin(273));
+				assert_equal(t22.end("lorem"), t22.begin("lorem"));
+			}
+
+
+			test( NoLinkedStatisticsIsProvidedForMissingEntries )
+			{
+				// INIT
+				map11_t m;
+				callees_transform<map11_t> t(m);
+
+				// ACT / ASSERT
+				assert_equal(t.end(314), t.begin(314));
+			}
+
+
+			test( NestedContainerIteratorsAreProvidedAsRange )
+			{
+				// INIT
+				map11_t m;
+				callees_transform<map11_t> t(m);
+
+				m[150].callees[1] = "a";
+				m[150].callees[2] = "b";
+				m[151].callees[3] = "A";
+				m[151].callees[7] = "B";
+				m[151].callees[8] = "B";
+
+				// ACT / ASSERT
+				assert_equal(m[150].callees.begin(), t.begin(150));
+				assert_equal(m[150].callees.end(), t.end(150));
+				assert_equal(m[151].callees.begin(), t.begin(151));
+				assert_equal(m[151].callees.end(), t.end(151));
+			}
+		end_test_suite
+
+
+		begin_test_suite( CallersStatisticsTransformTests )
+			typedef map<int, statistics1_t> map11_t;
+			typedef unordered_map<string, statistics1_t> map12_t;
+			typedef map<int, statistics2_t> map21_t;
+			typedef map<string, statistics2_t> map22_t;
+
+			test( NoLinkedStatisticsIsProvidedForEmptyCallers )
+			{
+				// INIT
+				map11_t m11;
+				map12_t m12;
+				map21_t m21;
+				map22_t m22;
+
+				m11[314];
+				m12["abc"];
+				m21[273];
+				m22["lorem"];
+
+				// INIT / ACT
+				callers_transform<map11_t> t11(m11);
+				callers_transform<map12_t> t12(m12);
+				callers_transform<map21_t> t21(m21);
+				callers_transform<map22_t> t22(m22);
+
+				// ACT / ASSERT
+				assert_equal(t11.end(314), t11.begin(314));
+				assert_equal(t12.end("abc"), t12.begin("abc"));
+				assert_equal(t21.end(273), t21.begin(273));
+				assert_equal(t22.end("lorem"), t22.begin("lorem"));
+			}
+
+
+			test( NoLinkedStatisticsIsProvidedForMissingEntries )
+			{
+				// INIT
+				map11_t m;
+				callers_transform<map11_t> t(m);
+
+				// ACT / ASSERT
+				assert_equal(t.end(314), t.begin(314));
+			}
+
+
+			test( NestedContainerIteratorsAreProvidedAsRange )
+			{
+				// INIT
+				map11_t m;
+				callers_transform<map11_t> t(m);
+
+				m[150].callers.push_back(0.1);
+				m[150].callers.push_back(1.1);
+				m[150].callers.push_back(3.1);
+				m[151].callers.push_back(7.1);
+				m[151].callers.push_back(9.1);
+
+				// ACT / ASSERT
+				assert_equal(m[150].callers.begin(), t.begin(150));
+				assert_equal(m[150].callers.end(), t.end(150));
+				assert_equal(m[151].callers.begin(), t.begin(151));
+				assert_equal(m[151].callers.end(), t.end(151));
+			}
+		end_test_suite
+
+	}
+}

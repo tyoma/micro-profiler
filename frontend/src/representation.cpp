e0dcaa230b6eb90c6b0a43de903703d53a2232fb
Artem G <artem.gevorkyan@gmail.com>
2022-06-25 18:43:59 -0600

    flat statistics representations added

--- frontend/src/representation.cpp
+++ frontend/src/representation.cpp
@@ -30,14 +30,21 @@ using namespace std;
 
 namespace micro_profiler
 {
+	namespace keyer
+	{
+		struct resetting_thread_id : thread_id
+		{
+			using thread_id::operator ();
+			id_t operator ()(const call_statistics &) const
+			{	return threads_model::cumulative;	}
+		};
+	}
+
 	namespace
 	{
-		namespace
-		{
-			template <typename T>
-			shared_ptr<T> make_shared_copy(const T &from)
-			{	return make_shared<T>(from);	}
-		}
+		template <typename T>
+		shared_ptr<T> make_shared_copy(const T &from)
+		{	return make_shared<T>(from);	}
 
 		struct sum_functions
 		{
@@ -70,72 +77,78 @@ namespace micro_profiler
 
 			return filtered_calls_statistics_table_cptr(composite, &*get<2>(*composite));
 		}
+
+		template <bool callstacks>
+		representation<callstacks, threads_all> create_all(calls_statistics_table_cptr main,
+			calls_statistics_table_cptr source)
+		{
+			const auto selection_main = make_shared<selector_table>();
+			const auto selection_main_addresses = derived_statistics::addresses(selection_main, source);
+			representation<callstacks, threads_all> r = {
+				main, selection_main,
+				derived_statistics::callers(selection_main_addresses, source),
+				derived_statistics::callees(selection_main_addresses, source),
+			};
+
+			return r;
+		}
+
+		template <bool callstacks>
+		representation<callstacks, threads_cumulative> create_cumulative(calls_statistics_table_cptr source)
+		{
+			auto from = representation<callstacks, threads_all>::create(group_by_callstack(source));
+			representation<callstacks, threads_cumulative> r = {	from.main, from.selection_main, from.callers, from.callees,	};
+
+			return r;
+		}
+
+		template <bool callstacks>
+		representation<callstacks, threads_filtered> create_filtered(calls_statistics_table_cptr source, id_t thread_id)
+		{
+			const auto from = representation<callstacks, threads_all>::create(source);
+			const auto selection_threads = make_shared<selector_table>();
+			representation<callstacks, threads_filtered> r = {
+				selection_threads,
+				join_by_thread(from.main, selection_threads), from.selection_main,
+				join_by_thread(from.callers, selection_threads),
+				join_by_thread(from.callees, selection_threads)
+			};
+			auto rec = selection_threads->create();
+
+			*rec = thread_id;
+			rec.commit();
+			return r;
+		}
+
 	}
 
 	template <>
 	representation<true, threads_all> representation<true, threads_all>::create(calls_statistics_table_cptr source)
-	{
-		const auto selection_main = make_shared<selector_table>();
-		const auto selection_main_addresses = derived_statistics::addresses(selection_main, source);
-		representation<true, threads_all> r = {
-			source, selection_main,
-			derived_statistics::callers(selection_main_addresses, source),
-			derived_statistics::callees(selection_main_addresses, source),
-		};
-
-		return r;
-	}
+	{	return create_all<true>(source, source);	}
 
 	template <>
-	representation<true, threads_cumulative> representation<true, threads_cumulative>::create(calls_statistics_table_cptr source_)
-	{
-		const auto source = group_by_callstack(source_);
-		const auto selection_main = make_shared<selector_table>();
-		const auto selection_main_addresses = derived_statistics::addresses(selection_main, source);
-		representation<true, threads_cumulative> r = {
-			source, selection_main,
-			derived_statistics::callers(selection_main_addresses, source),
-			derived_statistics::callees(selection_main_addresses, source),
-		};
-
-		return r;
-	}
+	representation<true, threads_cumulative> representation<true, threads_cumulative>::create(calls_statistics_table_cptr source)
+	{	return create_cumulative<true>(source);	}
 
 	template <>
 	representation<true, threads_filtered> representation<true, threads_filtered>::create(calls_statistics_table_cptr source, id_t thread_id)
-	{
-		const auto from = representation<true, threads_all>::create(source);
-		const auto selection_threads = make_shared<selector_table>();
-		representation<true, threads_filtered> r = {
-			selection_threads,
-			join_by_thread(from.main, selection_threads), from.selection_main,
-			join_by_thread(from.callers, selection_threads),
-			join_by_thread(from.callees, selection_threads)
-		};
-		auto rec = selection_threads->create();
-
-		*rec = thread_id;
-		rec.commit();
-		return r;
-	}
+	{	return create_filtered<true>(source, thread_id);	}
 
 	template <>
 	representation<false, threads_all> representation<false, threads_all>::create(calls_statistics_table_cptr source)
 	{
-		const auto selection_main = make_shared<selector_table>();
-		const auto selection_main_addresses = derived_statistics::addresses(selection_main, source);
-		const auto main_flat = views::group_by<call_statistics>(*source,
+		return create_all<false>(views::group_by<call_statistics>(*source,
 			[] (const calls_statistics_table &, views::agnostic_key_tag) {
 
 			return keyer::combine2<keyer::address, keyer::thread_id>();
-		}, auto_increment_constructor<call_statistics>(), aggregator::sum_flat(*source));
-		representation<false, threads_all> r = {
-			main_flat, selection_main,
-			derived_statistics::callers(selection_main_addresses, source),
-			derived_statistics::callees(selection_main_addresses, source),
-		};
-
-		return r;
+		}, auto_increment_constructor<call_statistics>(), aggregator::sum_flat(*source)), source);
 	}
 
+	template <>
+	representation<false, threads_cumulative> representation<false, threads_cumulative>::create(calls_statistics_table_cptr source)
+	{	return create_cumulative<false>(source);	}
+
+	template <>
+	representation<false, threads_filtered> representation<false, threads_filtered>::create(calls_statistics_table_cptr source, id_t thread_id)
+	{	return create_filtered<false>(source, thread_id);	}
 }
--- frontend/tests/RepresentationTests.cpp
+++ frontend/tests/RepresentationTests.cpp
@@ -32,6 +32,23 @@ namespace micro_profiler
 					+ make_call_statistics(9, 2, 8, 104, 1009, 0, 0, 0, 0));
 				return source;
 			}
+
+			shared_ptr<calls_statistics_table> create_sample_with_recursion()
+			{
+				auto source = make_shared<calls_statistics_table>();
+
+				add_records(*source, plural
+					+ make_call_statistics(1, 1, 0, 101, 1011, 0, 11, 0, 0)
+					+ make_call_statistics(2, 1, 1, 102, 1002, 0, 13, 0, 0)
+					+ make_call_statistics(3, 1, 1, 103, 1003, 0, 17, 0, 0)
+					+ make_call_statistics(4, 1, 3, 102, 1004, 0, 19, 0, 0)
+					+ make_call_statistics(5, 1, 3, 101, 1005, 0, 23, 0, 0)
+					+ make_call_statistics(6, 5, 0, 101, 1006, 0, 29, 0, 0)
+					+ make_call_statistics(7, 5, 6, 102, 1007, 0, 31, 0, 0)
+					+ make_call_statistics(8, 5, 7, 102, 1008, 0, 37, 0, 0)
+					+ make_call_statistics(9, 5, 8, 102, 1009, 0, 41, 0, 0));
+				return source;
+			}
 		}
 
 
@@ -324,19 +341,7 @@ namespace micro_profiler
 			shared_ptr<calls_statistics_table> source;
 
 			init( CreateSourceTable )
-			{
-				source = make_shared<calls_statistics_table>();
-				add_records(*source, plural
-					+ make_call_statistics(1, 1, 0, 101, 1011, 0, 11, 0, 0)
-					+ make_call_statistics(2, 1, 1, 102, 1002, 0, 13, 0, 0)
-					+ make_call_statistics(3, 1, 1, 103, 1003, 0, 17, 0, 0)
-					+ make_call_statistics(4, 1, 3, 102, 1004, 0, 19, 0, 0)
-					+ make_call_statistics(5, 1, 3, 101, 1005, 0, 23, 0, 0)
-					+ make_call_statistics(6, 5, 0, 101, 1006, 0, 29, 0, 0)
-					+ make_call_statistics(7, 5, 6, 102, 1007, 0, 31, 0, 0)
-					+ make_call_statistics(8, 5, 7, 102, 1008, 0, 37, 0, 0)
-					+ make_call_statistics(9, 5, 8, 102, 1009, 0, 41, 0, 0));
-			}
+			{	source = create_sample_with_recursion();	}
 
 
 			test( MainStatisticsIsRecursionAwareAggregationOfTheSource )
@@ -425,5 +430,210 @@ namespace micro_profiler
 					+ make_call_statistics(0, 5, 0, 102, 3024, 0, 31, 0, 0), *rep.callees);
 			}
 		end_test_suite
+
+
+		begin_test_suite( RepresentationFlatCumulativeTests )
+			shared_ptr<calls_statistics_table> source;
+
+			init( CreateSourceTable )
+			{	source = create_sample_with_recursion();	}
+
+
+			test( MainStatisticsIsRecursionAwareAggregationOfTheSource )
+			{
+				// INIT / ACT
+				auto rep = representation<false, threads_cumulative>::create(source);
+
+				// ACT / ASSERT
+				assert_not_null(rep.main);
+				assert_equivalent(plural
+					+ make_call_statistics(1, threads_model::cumulative, 0, 101, 3022, 0, 40, 0, 0)
+					+ make_call_statistics(2, threads_model::cumulative, 0, 102, 5030, 0, 63, 0, 0)
+					+ make_call_statistics(3, threads_model::cumulative, 0, 103, 1003, 0, 17, 0, 0), *rep.main);
+				assert_not_null(rep.selection_main);
+
+				// INIT
+				weak_ptr<calls_statistics_table> wsource = source;
+
+				// ACT
+				source.reset();
+
+				// ASSERT
+				assert_is_false(wsource.expired());
+			}
+
+
+			test( CallersAreListedAccordinglyToSelection )
+			{
+				// INIT / ACT
+				auto rep = representation<false, threads_cumulative>::create(source);
+
+				// ACT / ASSERT
+				assert_not_null(rep.callers);
+				assert_equal(rep.callers->end(), rep.callers->begin());
+
+				// ACT
+				add_records(*rep.selection_main, plural + 1u);
+
+				// ACT / ASSERT
+				assert_equivalent(plural
+					+ make_call_statistics(0, threads_model::cumulative, 0, 103, 1005, 0, 0, 0, 0)
+					+ make_call_statistics(0, threads_model::cumulative, 0, 0, 2017, 0, 40, 0, 0), *rep.callers);
+
+				// ACT
+				add_records(*rep.selection_main, plural + 2u);
+
+				// ACT / ASSERT
+				assert_equivalent(plural
+					+ make_call_statistics(0, threads_model::cumulative, 0, 0, 2017, 0, 40, 0, 0)
+					+ make_call_statistics(0, threads_model::cumulative, 0, 103, 2009, 0, 19, 0, 0)
+					+ make_call_statistics(0, threads_model::cumulative, 0, 101, 2009, 0, 44, 0, 0)
+					+ make_call_statistics(0, threads_model::cumulative, 0, 102, 2017, 0, 0, 0, 0), *rep.callers);
+			}
+
+
+			test( CalleesAreListedAccordinglyToSelection )
+			{
+				// INIT / ACT
+				auto rep = representation<false, threads_cumulative>::create(source);
+
+				// ACT / ASSERT
+				assert_not_null(rep.callees);
+				assert_equal(rep.callees->end(), rep.callees->begin());
+
+				// ACT
+				add_records(*rep.selection_main, plural + 1u);
+
+				// ACT / ASSERT
+				assert_equivalent(plural
+					+ make_call_statistics(0, threads_model::cumulative, 0, 102, 2009, 0, 44, 0, 0)
+					+ make_call_statistics(0, threads_model::cumulative, 0, 103, 1003, 0, 17, 0, 0), *rep.callees);
+
+				// ACT
+				add_records(*rep.selection_main, plural + 2u);
+
+				// ACT / ASSERT
+				assert_equivalent(plural
+					+ make_call_statistics(0, threads_model::cumulative, 0, 102, 4026, 0, 44, 0, 0)
+					+ make_call_statistics(0, threads_model::cumulative, 0, 103, 1003, 0, 17, 0, 0), *rep.callees);
+			}
+		end_test_suite
+
+
+		begin_test_suite( RepresentationFlatFilteredTests )
+			shared_ptr<calls_statistics_table> source;
+
+			init( CreateSourceTable )
+			{	source = create_sample_with_recursion();	}
+
+
+			test( MainStatisticsIsRecursionAwareAggregationOfTheSource )
+			{
+				// INIT / ACT
+				auto rep1 = representation<false, threads_filtered>::create(source, 1);
+
+				// ACT / ASSERT
+				assert_not_null(rep1.main);
+				assert_equivalent(plural
+					+ make_call_statistics(1, 1, 0, 101, 2016, 0, 11, 0, 0)
+					+ make_call_statistics(2, 1, 0, 102, 2006, 0, 32, 0, 0)
+					+ make_call_statistics(3, 1, 0, 103, 1003, 0, 17, 0, 0), *rep1.main);
+				assert_not_null(rep1.selection_main);
+
+				// INIT / ACT
+				auto rep2 = representation<false, threads_filtered>::create(source, 5);
+
+				// ACT / ASSERT
+				assert_equivalent(plural
+					+ make_call_statistics(4, 5, 0, 101, 1006, 0, 29, 0, 0)
+					+ make_call_statistics(5, 5, 0, 102, 3024, 0, 31, 0, 0), *rep2.main);
+
+				// INIT / ACT
+				add_records(*rep2.selection_threads, plural + 1u);
+
+				// ACT / ASSERT
+				assert_equivalent(plural
+					+ make_call_statistics(1, 1, 0, 101, 2016, 0, 11, 0, 0)
+					+ make_call_statistics(2, 1, 0, 102, 2006, 0, 32, 0, 0)
+					+ make_call_statistics(3, 1, 0, 103, 1003, 0, 17, 0, 0)
+					+ make_call_statistics(4, 5, 0, 101, 1006, 0, 29, 0, 0)
+					+ make_call_statistics(5, 5, 0, 102, 3024, 0, 31, 0, 0), *rep2.main);
+
+				// INIT
+				weak_ptr<calls_statistics_table> wsource = source;
+
+				// ACT
+				source.reset();
+
+				// ASSERT
+				assert_is_false(wsource.expired());
+			}
+
+
+			test( CallersAreListedAccordinglyToSelection )
+			{
+				// INIT / ACT
+				auto rep = representation<false, threads_filtered>::create(source, 1);
+
+				// ACT / ASSERT
+				assert_not_null(rep.callers);
+				assert_equal(rep.callers->end(), rep.callers->begin());
+
+				// ACT
+				add_records(*rep.selection_main, plural + 1u);
+
+				// ACT / ASSERT
+				assert_equivalent(plural
+					+ make_call_statistics(0, 1, 0, 0, 1011, 0, 11, 0, 0)
+					+ make_call_statistics(0, 1, 0, 103, 1005, 0, 0, 0, 0), *rep.callers);
+
+				// ACT
+				add_records(*rep.selection_threads, plural + 5u);
+
+				// ACT / ASSERT
+				assert_equivalent(plural
+					+ make_call_statistics(0, 1, 0, 0, 1011, 0, 11, 0, 0)
+					+ make_call_statistics(0, 1, 0, 103, 1005, 0, 0, 0, 0)
+					+ make_call_statistics(0, 5, 0, 0, 1006, 0, 29, 0, 0), *rep.callers);
+			}
+
+
+			test( CalleesAreListedAccordinglyToSelection )
+			{
+				// INIT / ACT
+				auto rep = representation<false, threads_filtered>::create(source, 1);
+
+				// ACT / ASSERT
+				assert_not_null(rep.callees);
+				assert_equal(rep.callees->end(), rep.callees->begin());
+
+				// ACT
+				add_records(*rep.selection_main, plural + 1u);
+
+				// ACT / ASSERT
+				assert_equivalent(plural
+					+ make_call_statistics(0, 1, 0, 102, 1002, 0, 13, 0, 0)
+					+ make_call_statistics(0, 1, 0, 103, 1003, 0, 17, 0, 0), *rep.callees);
+
+				// ACT
+				add_records(*rep.selection_threads, plural + 5u);
+
+				// ACT / ASSERT
+				assert_equivalent(plural
+					+ make_call_statistics(0, 1, 0, 102, 1002, 0, 13, 0, 0)
+					+ make_call_statistics(0, 1, 0, 103, 1003, 0, 17, 0, 0)
+					+ make_call_statistics(0, 5, 0, 102, 1007, 0, 31, 0, 0), *rep.callees);
+
+				// ACT
+				add_records(*rep.selection_main, plural + 2u);
+
+				// ACT / ASSERT
+				assert_equivalent(plural
+					+ make_call_statistics(0, 1, 0, 102, 1002, 0, 13, 0, 0)
+					+ make_call_statistics(0, 1, 0, 103, 1003, 0, 17, 0, 0)
+					+ make_call_statistics(0, 5, 0, 102, 3024, 0, 31, 0, 0), *rep.callees);
+			}
+		end_test_suite
+
 	}
 }

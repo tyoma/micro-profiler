2d80aad47d582088f171a4d1726897aaf8340135
Artem G <artem.gevorkyan@gmail.com>
2023-05-17 20:02:14 -0600

    gap_search_* algorithms added required for a reachable_allocator

---
+++ common/gap_searcher.h
@@ -0,0 +1,88 @@
+//	Copyright (c) 2011-2022 by Artem A. Gevorkyan (gevorkyan.org)
+//
+//	Permission is hereby granted, free of charge, to any person obtaining a copy
+//	of this software and associated documentation files (the "Software"), to deal
+//	in the Software without restriction, including without limitation the rights
+//	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//	copies of the Software, and to permit persons to whom the Software is
+//	furnished to do so, subject to the following conditions:
+//
+//	The above copyright notice and this permission notice shall be included in
+//	all copies or substantial portions of the Software.
+//
+//	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//	THE SOFTWARE.
+
+#pragma once
+
+#include <algorithm>
+#include <iterator>
+#include <type_traits>
+#include <utility>
+#include <vector>
+
+namespace micro_profiler
+{
+	struct location_length_pair_less
+	{
+		template <typename T1, typename T2, typename T3>
+		bool operator ()(const std::pair<T1, T2> &lhs, T3 rhs) const
+		{	return lhs.first < rhs;	}
+
+		template <typename T1, typename T2, typename T3>
+		bool operator ()(T1 lhs, const std::pair<T2, T3> &rhs) const
+		{	return lhs < rhs.first;	}
+
+		template <typename T1, typename T2, typename T3, typename T4>
+		bool operator ()(const std::pair<T1, T2> &lhs, const std::pair<T3, T4> &rhs) const
+		{	return lhs.first < rhs.first;	}
+
+		template <typename T, typename T2>
+		static T region_start(const std::pair<T, T2> &value)
+		{	return value.first;	}
+
+		template <typename T, typename T2>
+		static T region_end(const std::pair<T, T2> &value)
+		{	return value.first + value.second;	}
+	};
+
+
+
+	template <typename T, typename V, typename P>
+	typename std::vector<T>::const_iterator lower_bound(const std::vector<T> &map, const V &value, const P &pred)
+	{	return std::lower_bound(std::begin(map), std::end(map), value, pred);	}
+
+	template <typename T, typename V, typename P>
+	typename std::vector<T>::const_iterator upper_bound(const std::vector<T> &map, const V &value, const P &pred)
+	{	return std::upper_bound(std::begin(map), std::end(map), value, pred);	}
+
+	template <typename L, typename T, typename S, typename P>
+	inline bool gap_search_up(L &location, const T &map, S min_gap_size, const P &pred)
+	{
+		const auto reference = location;
+
+		for (auto i = lower_bound(map, location, pred);
+			i != std::end(map) && (pred.region_start(*i) < location + min_gap_size);
+			location = pred.region_end(*i++))
+		{	}
+		return reference <= location + min_gap_size - 1;
+	}
+
+	template <typename L, typename T, typename S, typename P>
+	inline bool gap_search_down(L &location, const T &map, S min_gap_size, const P &pred)
+	{
+		const auto reference = location;
+
+		location -= min_gap_size;
+		for (auto i = upper_bound(map, location, pred);
+			i != std::begin(map) && pred.region_end(*--i) > location;
+			location = pred.region_start(*i) - min_gap_size)
+		{	}
+		return location <= reference;
+	}
+}
---
+++ common/tests/GapSearcherTests.cpp
@@ -0,0 +1,335 @@
+#include <common/gap_searcher.h>
+
+#include <common/types.h>
+#include <test-helpers/helpers.h>
+#include <ut/assert.h>
+#include <ut/test.h>
+
+using namespace std;
+
+namespace micro_profiler
+{
+	namespace tests
+	{
+		begin_test_suite( GapSearcherTests )
+			//test( RegionPastRequestedIsReturnedForEmptyMap )
+			//{
+			//	// INIT
+			//	vector< pair<int, int> > m1;
+			//	vector< pair<byte *, int> > m2;
+
+			//	// ACT
+			//	int r1 = gap_search(m1, make_pair(10, 100), 53 /*size*/, 13 /*max_distance_bits*/, location_length_pair_less());
+			//	byte *r2 = gap_search(m2, make_pair((byte *)0x11391100, 16384), 1 << 16, 18, location_length_pair_less());
+
+			//	// ASSERT
+			//	assert_equal(110, r1);
+			//	assert_equal((byte *)(0x11391100 + 16384), r2);
+			//}
+
+
+			test( GapSearchUpReturnsFirstMatchNoGaps )
+			{
+				// INIT
+				int location;
+				vector< pair<int, int> > m;
+
+				// ACT / ASSERT
+				location = 110;
+				assert_is_true(gap_search_up(location, m, 53 /*size*/, location_length_pair_less()));
+
+				// ASSERT
+				assert_equal(110, location);
+
+				// INIT
+				m.push_back(make_pair(110, 103));
+
+				// ACT / ASSERT
+				location = 110;
+				assert_is_true(gap_search_up(location, m, 53 /*size*/, location_length_pair_less()));
+
+				// ASSERT
+				assert_equal(213, location);
+
+				// INIT
+				m.push_back(make_pair(213, 1000));
+
+				// ACT
+				location = 110;
+				assert_is_true(gap_search_up(location, m, 53 /*size*/, location_length_pair_less()));
+
+				// ASSERT
+				assert_equal(1213, location);
+
+				// ACT
+				location = numeric_limits<int>::max() - 115;
+				assert_is_true(gap_search_up(location, m, 116 /*size*/, location_length_pair_less()));
+
+				// ASSERT
+				assert_equal(numeric_limits<int>::max() - 115, location);
+			}
+
+
+			test( GapSearchUpFailsOnOverflow )
+			{
+				// ACT / ASSERT
+				auto ilocation = numeric_limits<int>::max() - 49;
+				assert_is_false(gap_search_up(ilocation, vector< pair<int, int> >(), 51 /*size*/, location_length_pair_less()));
+
+				auto plocation = (byte *)(numeric_limits<size_t>::max() - 50);
+				assert_is_false(gap_search_up(plocation, vector< pair<byte *, int> >(), 52 /*size*/, location_length_pair_less()));
+			}
+
+
+			test( GapSearchUpReturnsFirstMatchNoGapsPtr )
+			{
+				// INIT
+				byte *location;
+				vector< pair<byte *, int> > m;
+
+				// ACT / ASSERT
+				location = (byte *)110;
+				assert_is_true(gap_search_up(location, m, 53 /*size*/, location_length_pair_less()));
+
+				// ASSERT
+				assert_equal((byte *)110, location);
+
+				// INIT
+				m.push_back(make_pair((byte *)110, 103));
+
+				// ACT / ASSERT
+				location = (byte *)110;
+				assert_is_true(gap_search_up(location, m, 53 /*size*/, location_length_pair_less()));
+
+				// ASSERT
+				assert_equal((byte *)213, location);
+
+				// INIT
+				m.push_back(make_pair((byte *)213, 1000));
+
+				// ACT
+				location = (byte *)110;
+				assert_is_true(gap_search_up(location, m, 53 /*size*/, location_length_pair_less()));
+
+				// ASSERT
+				assert_equal((byte *)1213, location);
+
+				// ACT
+				location = (byte *)(numeric_limits<size_t>::max() - 115);
+				assert_is_true(gap_search_up(location, m, 116 /*size*/, location_length_pair_less()));
+
+				// ASSERT
+				assert_equal((byte *)(numeric_limits<size_t>::max() - 115), location);
+			}
+
+
+			test( GapSearchUpReturnsFirstMatchWithGaps )
+			{
+				// INIT
+				int location;
+				auto m = plural
+					+ make_pair(1000, 100)
+					+ make_pair(1150, 230)
+					+ make_pair(1480, 520)
+					+ make_pair(2200, 230);
+
+				// ACT / ASSERT
+				location = 999;
+				assert_is_true(gap_search_up(location, m, 1 /*size*/, location_length_pair_less()));
+				assert_equal(999, location);
+				location = 999;
+				assert_equal(1100, (gap_search_up(location, m, 2 /*size*/, location_length_pair_less()), location));
+				location = 999;
+				assert_equal(1100, (gap_search_up(location, m, 50 /*size*/, location_length_pair_less()), location));
+				location = 999;
+				assert_equal(1380, (gap_search_up(location, m, 51 /*size*/, location_length_pair_less()), location));
+				location = 999;
+				assert_equal(1380, (gap_search_up(location, m, 100 /*size*/, location_length_pair_less()), location));
+				location = 999;
+				assert_equal(2000, (gap_search_up(location, m, 101 /*size*/, location_length_pair_less()), location));
+				location = 999;
+				assert_equal(2000, (gap_search_up(location, m, 200 /*size*/, location_length_pair_less()), location));
+				location = 999;
+				assert_equal(2430, (gap_search_up(location, m, 201 /*size*/, location_length_pair_less()), location));
+
+				location = 1100;
+				assert_equal(1100, (gap_search_up(location, m, 1 /*size*/, location_length_pair_less()), location));
+				location = 1100;
+				assert_equal(1100, (gap_search_up(location, m, 2 /*size*/, location_length_pair_less()), location));
+				location = 1100;
+				assert_equal(1100, (gap_search_up(location, m, 50 /*size*/, location_length_pair_less()), location));
+				location = 1100;
+				assert_equal(1380, (gap_search_up(location, m, 51 /*size*/, location_length_pair_less()), location));
+				location = 1100;
+				assert_equal(2430, (gap_search_up(location, m, 201 /*size*/, location_length_pair_less()), location));
+				location = 1100;
+				assert_is_true(gap_search_up(location, m, 201 /*size*/, location_length_pair_less()));
+				assert_equal(2430, location);
+			}
+
+
+			test( GapSearchDownReturnsFirstMatchNoGaps )
+			{
+				// INIT
+				int location;
+				vector< pair<int, int> > m;
+
+				// ACT
+				location = 1000;
+				assert_is_true(gap_search_down(location, m, 53 /*size*/, location_length_pair_less()));
+
+				// ASSERT
+				assert_equal(947, location);
+
+				// INIT
+				m.insert(m.begin(), make_pair(910, 90));
+
+				// ACT
+				location = 1000;
+				assert_is_true(gap_search_down(location, m, 73 /*size*/, location_length_pair_less()));
+
+				// ASSERT
+				assert_equal(837, location);
+
+				// INIT
+				m.insert(m.begin(), make_pair(760, 150));
+
+				// ACT
+				location = 1000;
+				assert_is_true(gap_search_down(location, m, 53 /*size*/, location_length_pair_less()));
+
+				// ASSERT
+				assert_equal(707, location);
+
+				// ACT
+				location = numeric_limits<int>::min() + 760;
+				assert_is_true(gap_search_down(location, m, 760 /*size*/, location_length_pair_less()));
+
+				// ASSERT
+				assert_equal(numeric_limits<int>::min(), location);
+			}
+
+
+			test( GapSearchDownFailsOnOverflow )
+			{
+				// ACT / ASSERT
+				auto ilocation = numeric_limits<int>::min() + 50;
+				assert_is_false(gap_search_down(ilocation, vector< pair<int, int> >(), 51 /*size*/, location_length_pair_less()));
+
+				auto plocation = (byte *)57;
+				assert_is_false(gap_search_down(plocation, vector< pair<byte *, int> >(), 58 /*size*/, location_length_pair_less()));
+			}
+
+
+			test( GapSearchDownReturnsFirstMatchNoGapsPtr )
+			{
+				// INIT
+				byte *location;
+				vector< pair<byte *, int> > m;
+
+				// ACT
+				location = (byte *)1000;
+				assert_is_true(gap_search_down(location, m, 53 /*size*/, location_length_pair_less()));
+
+				// ASSERT
+				assert_equal((byte *)947, location);
+
+				// INIT
+				m.insert(m.begin(), make_pair((byte *)910, 90));
+
+				// ACT
+				location = (byte *)1000;
+				assert_is_true(gap_search_down(location, m, 73 /*size*/, location_length_pair_less()));
+
+				// ASSERT
+				assert_equal((byte *)837, location);
+
+				// INIT
+				m.insert(m.begin(), make_pair((byte *)760, 150));
+
+				// ACT
+				location = (byte *)1000;
+				assert_is_true(gap_search_down(location, m, 53 /*size*/, location_length_pair_less()));
+
+				// ASSERT
+				assert_equal((byte *)707, location);
+
+				// ACT
+				location = (byte *)760;
+				assert_is_true(gap_search_down(location, m, 760 /*size*/, location_length_pair_less()));
+
+				// ASSERT
+				assert_equal(nullptr, location);
+			}
+
+
+			test( GapSearchDownReturnsFirstMatchWithGaps )
+			{
+				// INIT
+				int location;
+				auto m = plural
+					+ make_pair(10000, 500) // followed by a 1000-units gap
+					+ make_pair(11500, 3100) // followed by a 200-units gap
+					+ make_pair(14800, 7100); // followed by a 100-units gap
+//					+ make_pair(22000, 230);
+
+				// ACT / ASSERT
+				location = 22000;
+				assert_is_true(gap_search_down(location, m, 0 /*size*/, location_length_pair_less()));
+				assert_equal(22000, location);
+				location = 22000;
+				assert_equal(21999, (gap_search_down(location, m, 1 /*size*/, location_length_pair_less()), location));
+				location = 22000;
+				assert_equal(21998, (gap_search_down(location, m, 2 /*size*/, location_length_pair_less()), location));
+				location = 22000;
+				assert_equal(21900, (gap_search_down(location, m, 100 /*size*/, location_length_pair_less()), location));
+				location = 22000;
+				assert_equal(14699, (gap_search_down(location, m, 101 /*size*/, location_length_pair_less()), location));
+				location = 22000;
+				assert_equal(14600, (gap_search_down(location, m, 200 /*size*/, location_length_pair_less()), location));
+				location = 22000;
+				assert_equal(11299, (gap_search_down(location, m, 201 /*size*/, location_length_pair_less()), location));
+				location = 22000;
+				assert_equal(10500, (gap_search_down(location, m, 1000 /*size*/, location_length_pair_less()), location));
+				location = 22000;
+				assert_is_true(gap_search_down(location, m, 1001 /*size*/, location_length_pair_less()));
+				assert_equal(8999, location);
+
+				// INIT
+				m = m + make_pair(22000, 230);
+
+				// ACT / ASSERT
+				location = 22000;
+				assert_equal(22000, (gap_search_down(location, m, 0 /*size*/, location_length_pair_less()), location));
+				location = 22000;
+				assert_equal(21999, (gap_search_down(location, m, 1 /*size*/, location_length_pair_less()), location));
+				location = 22000;
+				assert_equal(21998, (gap_search_down(location, m, 2 /*size*/, location_length_pair_less()), location));
+				location = 22000;
+				assert_equal(21900, (gap_search_down(location, m, 100 /*size*/, location_length_pair_less()), location));
+				location = 22000;
+				assert_equal(14699, (gap_search_down(location, m, 101 /*size*/, location_length_pair_less()), location));
+				location = 22000;
+				assert_equal(14600, (gap_search_down(location, m, 200 /*size*/, location_length_pair_less()), location));
+				location = 22000;
+				assert_equal(11299, (gap_search_down(location, m, 201 /*size*/, location_length_pair_less()), location));
+				location = 22000;
+				assert_equal(10500, (gap_search_down(location, m, 1000 /*size*/, location_length_pair_less()), location));
+				location = 22000;
+				assert_equal(8999, (gap_search_down(location, m, 1001 /*size*/, location_length_pair_less()), location));
+
+				location = 14900;
+				assert_equal(14799, (gap_search_down(location, m, 1 /*size*/, location_length_pair_less()), location));
+				location = 14900;
+				assert_equal(14798, (gap_search_down(location, m, 2 /*size*/, location_length_pair_less()), location));
+				location = 14900;
+				assert_equal(14700, (gap_search_down(location, m, 100 /*size*/, location_length_pair_less()), location));
+			}
+
+
+//			test( SearchDownReturns )
+
+		end_test_suite
+	}
+
+}

2874c2554365461d36c958e9a1541214b39bd123
Artem G <artem.gevorkyan@gmail.com>
2025-01-19 21:42:21 -0700

    Build procedure migrated to Conan 2.*

--- build.props/sqlite3.include.cmake
+++
@@ -1,8 +0,0 @@
-include(conan)
-
-conan_cmake_run(REQUIRES sqlite3/3.40.0
-	BUILD missing
-	BASIC_SETUP
-	CMAKE_TARGETS
-	ARCH ${MP_TARGET_ARCH}
-	OPTIONS sqlite3:shared=False;sqlite3:build_executable=False;sqlite3:threadsafe=2)

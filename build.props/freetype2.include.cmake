7d87e5780bcc4ec0921c4d6daa2325c1ba2b8228
Artem G. <artem.gevorkyan@gmail.com>
2025-03-11 15:51:15 -0600

    Build procedure migrated to Conan 2.*

--- build.props/freetype2.include.cmake
+++
@@ -1,8 +0,0 @@
-include(conan)
-
-conan_cmake_run(REQUIRES freetype/2.12.1
-	BUILD missing
-	BASIC_SETUP
-	CMAKE_TARGETS
-	ARCH ${MP_TARGET_ARCH}
-	OPTIONS freetype:shared=False;freetype:with_brotli=False;freetype:with_bzip2=False;freetype:with_png=False;freetype:with_zlib=False)
